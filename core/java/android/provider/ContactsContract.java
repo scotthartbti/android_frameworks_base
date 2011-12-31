b95b4a5ca97a804c0a8962c938c47b3f3d807c76
Roman Birg <romanbirg@gmail.com>
2011-12-30 23:31:41 -0600

    Revert "Add Facebook contact sync support (1/2)"
    This reverts commit 084abf3339f172cddeaeed32bd1801ce374a88cf.

--- core/java/android/provider/ContactsContract.java
+++ core/java/android/provider/ContactsContract.java
@@ -772,8 +772,6 @@ public final class ContactsContract {
         public static final String SYNC3 = "sync3";
         /** Generic column for use by sync adapters. */
         public static final String SYNC4 = "sync4";
-         /** Facebook sync */
-        public static final String IS_RESTRICTED = "is_restricted";
     }
 
     /**
