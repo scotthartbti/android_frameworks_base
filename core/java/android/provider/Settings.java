f229e835c50f309d7c95a6b8cd1b3948e617cff3
DerTeufel <dominik-kassel@gmx.de>
2012-11-01 00:13:50 +0100

    more work to toggles
    Change-Id: I6fbbff5a03ea99db0056459ee2a4296748fc400e
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2927,6 +2927,20 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
 	/**
+         * Enable Notification Toggles Icon Color
+         *
+         * @hide
+         */
+        public static final String ENABLE_TOGGLE_COLORS = "enable_toggle_colors";
+
+         /**
+         * Enable Notification Toggles Bar
+         *
+         * @hide
+         */
+        public static final String ENABLE_TOGGLE_BAR = "enable_toggle_bar";
+
+	/**
          * Notification Toggles Icon Color (On)
          *
          * @hide
