8f06595d48a0eede008ecacd379a371c40691a3c
kufikugel <kufikugel@googlemail.com>
2013-01-01 04:47:59 +0100

    Frameworks: Add fast charge QS toggle and other
    this commit add
    - qs toggle
    - interactive reacting with pw toggle
    - option stays after reboot
    - option is hidden if kernel doesn't support
    - code cleanup
    
    Change-Id: I137720a7fe1acb7b36bec0f68ac0e5b6aa3fcbd0
    Signed-off-by: scotthartbti <scott@ubuntu.com>
    
    Conflicts:
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2933,6 +2933,12 @@ public final class Settings {
          */
         public static final String QUICK_TILES_BG_PRESSED_COLOR = "quick_tiles_bg_pressed_color";
 
+       /**
+        * Whether fcharge is enabled or not if kernel supports it
+        * @hide
+        */
+        public static final String FCHARGE_ENABLED = "fcharge_enabled";
+
         /**
          * Quick Settings Tile Content
          *
