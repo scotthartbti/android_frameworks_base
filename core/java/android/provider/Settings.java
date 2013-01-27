9c6bc8ccb8f1886211b0b40db4e63b0b8461ca3e
Lars Greiss <kufikugel@googlemail.com>
2013-01-27 01:18:16 +0100

    Frameworks: Status Bar transparency and color (1/2)
    This introduces status bar transparency and color changes based on an idea
    of Roman.
    
    - custom status bar color
    - custom status bar transparency
    - custom status bar transparency mode (launcher/launcher+keyguard/always)
    - reset to default
    
    Patchset 2
    - cleanup
    
    Patchset 3
    - default color should not be red lmao
    
    cheers
    
    Change-Id: I1a4caab5fb9900077dc385d645bdfe1234114e83
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3002,6 +3002,25 @@ public final class Settings {
         public static final String NOTIF_ALPHA = "notif_alpha";
 
 	/**
+         * Sets transparency of statusbar
+         * @hide
+         */
+        public static final String STATUS_BAR_ALPHA = "status_bar_alpha";
+
+        /**
+         * Sets transparency mode of statusbar
+         * 0 = only home, 1 = keyguard and home (default), 2 = always
+         * @hide
+         */
+        public static final String STATUS_BAR_ALPHA_MODE = "status_bar_alpha_mode";
+
+        /**
+         * Sets color of statusbar
+         * @hide
+         */
+        public static final String STATUS_BAR_COLOR = "status_bar_color";
+
+	/**
         * Sets the portrait background of notification drawer
         * @hide
         */
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1594,6 +1594,7 @@ public class PhoneStatusBar extends BaseStatusBar {
                 haltTicker();
             }
         }
+        mStatusBarView.updateBackgroundAlpha();
     }
 
     @Override
@@ -2374,6 +2375,7 @@ public class PhoneStatusBar extends BaseStatusBar {
 
     @Override
     public void topAppWindowChanged(boolean showMenu) {
+        mStatusBarView.updateBackgroundAlpha();
         if (DEBUG) {
             Slog.d(TAG, (showMenu?"showing":"hiding") + " the MENU button");
         }
