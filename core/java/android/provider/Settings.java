0ed02bae851b5a02857f1456102c88acc7f33456
Lars Greiss <kufikugel@googlemail.com>
2013-02-23 22:57:39 +0100

    Frameworks: Move LockBeforeUnlock -> Secure (1/2)
    not really needed because it does not touch the security system.
    But imo better from consistence so I decided to move it.
    
    Also found a duplicate in provider.Settings from first porting
    merges
    
    cheers
    
    Change-Id: I465d28973e480010bdc4966933e83cf6c0df9990
    Signed-off-by: scott <scott@ubuntu.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -960,6 +960,8 @@ public final class Settings {
             MOVED_TO_SECURE.add(Secure.LOCK_SHOW_ERROR_PATH);
             MOVED_TO_SECURE.add(Secure.LOCK_DOTS_VISIBLE);
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED);
+	    MOVED_TO_SECURE.add(Secure.LOCK_BEFORE_UNLOCK);
+            MOVED_TO_SECURE.add(Secure.LOCKSCREEN_UNSECURE_USED);
             MOVED_TO_SECURE.add(Secure.LOGGING_ID);
             MOVED_TO_SECURE.add(Secure.PARENTAL_CONTROL_ENABLED);
             MOVED_TO_SECURE.add(Secure.PARENTAL_CONTROL_LAST_UPDATE);
@@ -2595,12 +2597,6 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
-	/**
-         * Stores value for lock before unlock
-         * @hide
-         */
-        public static final String LOCK_BEFORE_UNLOCK = "lock_before_unlock";
-
         /**
          * Stores values for custom lockscreen targets
          * @hide
@@ -2613,12 +2609,6 @@ public final class Settings {
          * @hide
          */
         public static final String LOCKSCREEN_MUSIC_CONTROLS = "lockscreen_music_controls";
-
-	/**
-          * Stores used unsecure lockscreen for normal mode and unlock before unlock
-          * @hide
-          */
-         public static final String LOCKSCREEN_UNSECURE_USED = "lockscreen_unsecure_used";
 
         /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
@@ -5650,6 +5640,13 @@ public final class Settings {
          */
         public static final String LOCK_BEFORE_UNLOCK =
             "lock_before_unlock";
+
+	/**
+         * Stores used unsecure lockscreen for normal mode and unlock before unlock
+         * @hide
+         */
+        public static final String LOCKSCREEN_UNSECURE_USED =
+            "lockscreen_unsecure_used";
 
         /**
          * Determines the width and height of the LockPatternView widget
