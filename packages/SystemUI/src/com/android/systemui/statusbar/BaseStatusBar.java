647df354a35dabf686628c37363ac474e9aaac19
drcmda <drcmda@gmail.com>
2013-03-03 22:05:38 +0000

    PIE: one finger swipe-up for notifications
    Change-Id: Ie23bd852fc41827f0067202685e7a97c595c3ac8

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -494,7 +494,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 LayoutInflater inflater = (LayoutInflater) mContext
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
                 mContainer = (PieExpandPanel)inflater.inflate(R.layout.pie_expanded_panel, null);
-                mContainer.init(mPile);
+                mContainer.init(mPile, mContainer.findViewById(R.id.content_scroll));
                 mWindowManager.addView(mContainer, PieStatusPanel.getFlipPanelLayoutParams());
             }
 
@@ -516,6 +516,8 @@ public abstract class BaseStatusBar extends SystemUI implements
                     addPieInLocation(Gravity.BOTTOM);
                     break;
             }
+
+
         } else {
             mPieControlsTrigger = null;
             mPieControlPanel = null;
