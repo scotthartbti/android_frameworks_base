49cf0a46834136afadfd16a9b3973f4809e55c14
drcmda <drcmda@gmail.com>
2013-03-03 01:54:34 +0000

    PIE: /// is ready for prime time
    Change-Id: Ib494c229fc65c99daadf598e3e29003950d55e72

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -197,6 +197,10 @@ public abstract class BaseStatusBar extends SystemUI implements
         return null;
     }
 
+    public IStatusBarService getService() {
+        return mBarService;
+    }
+
     public NotificationData getNotificationData() {
         return mNotificationData;
     }
