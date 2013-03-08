9ea7bcda12e2f0842e83bf70451c9fc313c24e86
scotthartbti <scott.hart.bti@gmail.com>
2013-03-08 00:01:23 -0800

    Revert "Revert "Forgot to define string for QS customization.""
    This reverts commit de998d778040f1c2c2cc5a7f215cfe33ecab84d3.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2950,6 +2950,12 @@ public final class Settings {
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
         /**
+	* Vibrate when expanding notifications
+	* @hide
+	*/
+        public static final String VIBRATE_NOTIF_EXPAND = "vibrate_notif_expand";
+
+        /**
          * whether which Ram Usage Bar mode is used on recent switcher
          * 0 = none, 1 = only app use, 2 = app and cache use, 3 = app, cache and system use
          * @hide
