7171fea304177ce4eff7f086481960abc7fd727e
Lars Greiss <kufikugel@googlemail.com>
2013-03-09 22:04:01 +0100

    Dark UI: BeanStalk goes TRDS
    All credits to SlimRoms!
    
    Change-Id: I9089ee86587fb33706eb5a4dcc8f38427336b25e
    Signed-off-by: Scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/UiModeManagerService.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/UiModeManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5659,6 +5659,13 @@ public final class Settings {
          */
         public static final String UI_NIGHT_MODE = "ui_night_mode";
 
+	/**
+         * Whether user activated inverted UI mode or default UI mode. Owned
+         * and controlled by UiModeManagerService.
+         * @hide
+         */
+        public static final String UI_INVERTED_MODE = "ui_inverted_mode";
+
         /**
          * Whether screensavers are enabled.
          * @hide
@@ -5803,7 +5810,8 @@ public final class Settings {
             MOUNT_UMS_NOTIFY_ENABLED,
             UI_NIGHT_MODE,
             ADVANCED_REBOOT,
-            PRIVACY_GUARD_DEFAULT
+            PRIVACY_GUARD_DEFAULT,
+	    UI_INVERTED_MODE
         };
 
         /**
