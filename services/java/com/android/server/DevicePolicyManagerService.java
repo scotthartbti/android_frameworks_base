3cd274f578c70dc756e503b92427617916612219
Robert Craig <rpcraig@tycho.ncsc.mil>
2013-03-18 06:30:15 -0400

    Move seinfo and install-time changes to inside PMS.
    Per request by Google, move the install-time checks entirely
    inside PMS.
    
    Change-Id: I01f2a9084dfe7886087b1497070b0d7f2ad8479e
    Signed-off-by: scott <scott@ubuntu.com>
    
    Conflicts:
    	services/java/com/android/server/DevicePolicyManagerService.java
    	services/java/com/android/server/pm/PackageManagerService.java

--- services/java/com/android/server/DevicePolicyManagerService.java
+++ services/java/com/android/server/DevicePolicyManagerService.java
@@ -49,7 +49,6 @@ import android.content.pm.Signature;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
-import android.content.pm.SELinuxMMAC;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -266,6 +265,8 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
             return true;
         }
     }
+
+    private static final String MMAC_ENFORCE_PROPERTY = "persist.mmac.enforce";
 
     private static final String SEPOLICY_PATH_SEPOLICY = "/data/security/sepolicy";
 
@@ -3130,12 +3131,12 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
             return false;
         }
 
-        boolean systemState = SELinuxMMAC.getEnforcingMode();
+        boolean systemState = SystemProperties.getBoolean(MMAC_ENFORCE_PROPERTY, false);
         boolean enforceMMAC = mmacAdmin.enforceMMAC;
         if (!firstBoot || !systemState) {
             if (systemState != enforceMMAC) {
                 Slog.v(TAG, "Changed MMAC enforcing status " + systemState + " to " + enforceMMAC);
-                SELinuxMMAC.setEnforcingMode(enforceMMAC);
+                SystemProperties.set(MMAC_ENFORCE_PROPERTY, enforceMMAC ? "true" : "false");
             }
         }
 
