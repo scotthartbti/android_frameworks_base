4db4a97d43622c4f628370330881b53016b79bb7
scotthartbti <scott.hart.bti@gmail.com>
2013-03-26 18:19:10 -0400

    Revert "Fix up expanded desktop behaviour."
    This reverts commit 667cdc82096a82d44c59914cdcd5652549b5fce1.

--- services/java/com/android/server/wm/WindowState.java
+++ services/java/com/android/server/wm/WindowState.java
@@ -498,7 +498,7 @@ final class WindowState implements WindowManagerPolicy.WindowState {
             mCompatFrame.scale(mInvGlobalScale);
         }
 
-        if (mIsWallpaper) {
+        if (mIsWallpaper && (fw != frame.width() || fh != frame.height())) {
             final DisplayInfo displayInfo = mDisplayContent.getDisplayInfo();
             mService.updateWallpaperOffsetLocked(this, displayInfo.appWidth, displayInfo.appHeight,
                     false);
