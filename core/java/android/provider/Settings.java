2a2eb738d800b7356dd669032c7f7629cc13fa2f
Muhammed Nazim <mnm9994u@gmail.com>
2013-04-16 19:03:00 +0200

    Classic Recents [1/2]
    Makes the recents menu like the one from GB.
    
    PS: Code exists in Android base. This commit just invokes it.
    
    Change-Id: I878b4ef70c74ce06884b4e0778c28d4caab08548
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3018,6 +3018,12 @@ public final class Settings {
          */
         public static final String DISABLE_FULLSCREEN_KEYBOARD = "disable_fullscreen_keyboard";
 
+        /**
+         * Select Classic Recents menu style [GB-]
+         * @hide
+         */
+        public static final String CLASSIC_RECENTS_MENU = "classic_recents_menu";
+
          /**
          * Statusbar State
          * @hide
