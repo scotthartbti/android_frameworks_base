54635f07e6bfa5a7dfcd78efd8808dedfc3bd1c5
Lars Greiss <kufikugel@googlemail.com>
2013-04-17 20:11:10 +0200

    Frameworks: Race conditions can be funny
    especially on slower phones. Whatever this commit
    changes the disable logic for PIE on lockscreen.
    With this correction SlimPIE is now unbreakable
    lmao
    
    cheers
    
    Change-Id: I442f771b57f1cbeb661418dea17ca0f4362428c5
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -255,7 +255,7 @@ public class PhoneStatusBarView extends PanelBar {
         mLastFullyOpenedPanel = null;
 
         // show up you pie controls
-        mBar.setupTriggers(false);
+        mBar.setupTriggers(false, false); 
     }
 
     @Override
@@ -267,7 +267,7 @@ public class PhoneStatusBarView extends PanelBar {
         }
 
         // back off you pie controls!
-        mBar.setupTriggers(true);
+        mBar.setupTriggers(true, false); 
 
         mFadingPanel = openPanel;
         mLastFullyOpenedPanel = openPanel;
