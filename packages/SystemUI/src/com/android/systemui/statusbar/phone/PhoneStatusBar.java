3ed3cdc732e769a77a4a797bf1aebf8131e11bfa
scotthartbti <scott.hart.bti@gmail.com>
2013-05-10 22:09:49 -0400

    Oops... ok... QS toggle working as it should now. Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    Change-Id: Iec742c222d459d349a1d2e6b7ef1703fb2db4f86

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -937,6 +937,15 @@ public class PhoneStatusBar extends BaseStatusBar {
     public void toggleQSShade() {
         int msg = 0;
         if (mHasFlipSettings) {
+                Settings.System.putInt(mContext.getContentResolver(),
+                        Settings.System.TOGGLE_NOTIFICATION_AND_QS_SHADE,
+                        (mExpandedVisible && !mNotificationPanelIsOpen) ? 0 : 1);
+
+                msg = (mExpandedVisible)
+                    ? ((mNotificationPanelIsOpen) ? MSG_FLIP_TO_QS_PANEL
+                    : MSG_CLOSE_PANELS) : MSG_OPEN_QS_PANEL;
+        } else { 
+
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.TOGGLE_NOTIFICATION_AND_QS_SHADE,
                     (mExpandedVisible) ? 0 : 1);
