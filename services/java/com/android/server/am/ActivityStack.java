d9ba3f0f1eeb3a241581fb0967756c868f5b4b8b
PrimeDirective <activethrasher00@gmail.com>
2013-06-06 21:14:45 -0400

    app loading: boost boost hints
    Change-Id: I56b49c61ccfa258880b7104470f9c2cdf4542184

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -1440,7 +1440,7 @@ final class ActivityStack {
 
     final boolean resumeTopActivityLocked(ActivityRecord prev, Bundle options) {
 
-        mPm.cpuBoost(1500000);
+        mPm.cpuBoost(1550000);
 
         // Find the first activity that is not finishing.
         ActivityRecord next = topRunningActivityLocked(null);
@@ -2503,7 +2503,7 @@ final class ActivityStack {
 
         int err = ActivityManager.START_SUCCESS;
 
-        mPm.cpuBoost(1500000);
+        mPm.cpuBoost(1550000);
 
         ProcessRecord callerApp = null;
         if (caller != null) {
