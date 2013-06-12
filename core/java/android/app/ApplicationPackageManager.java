9da65757c61b689cc9ca2e813fd6832e2f8d20dd
Steve Kondik <shade@chemlab.org>
2013-06-12 00:47:55 -0700

    framework: Privacy Guard
     * Introduce a new privacy feature which allows the user to run an
       application with reduced visibility into his or her personal data.
     * Adds a per-application flag and simple API to determine if this flag
       is enabled for the current or calling process.
     * This flag can be used by content providers to decide if they should
       return a limited/empty dataset.
    
    Change-Id: Id7c54d728e63acb2b02a2a9322930b54949f6c5d
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>
    
    Conflicts:
    	core/java/android/app/ApplicationPackageManager.java
    	core/res/res/values/cm_strings.xml
    	core/res/res/values/symbols.xml

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -48,6 +48,7 @@ import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Binder;
 import android.os.Process;
 import android.os.RemoteException;
 import android.util.Log;
@@ -1297,6 +1298,7 @@ final class ApplicationPackageManager extends PackageManager {
     }
 
     @Override
+
     public String[] getRevokedPermissions(String packageName) {
         try {
             return mPM.getRevokedPermissions(packageName);
@@ -1313,6 +1315,24 @@ final class ApplicationPackageManager extends PackageManager {
         } catch (RemoteException e) {
             // Should never happen!
         }
+    }
+
+    public void setPrivacyGuardSetting(String packageName, boolean enabled) {
+        try {
+            mPM.setPrivacyGuardSetting(packageName, enabled, mContext.getUserId());
+        } catch (RemoteException e) {
+            // Should never happen!
+        }
+    }
+
+    @Override
+    public boolean getPrivacyGuardSetting(String packageName) {
+        try {
+            return mPM.getPrivacyGuardSetting(packageName, mContext.getUserId());
+        } catch (RemoteException e) {
+            // Should never happen!
+        }
+        return false;
     }
 
     /**
