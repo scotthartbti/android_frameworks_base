e947fce1d755698e3070be0c2082c7de13fc0c05
Evan Anderson <evan1124@gmail.com>
2013-07-30 00:52:46 -0500

    Quick Settings: Option to use floating window (2/2)
    Allows the option of launching preferences from the quicksettings menu in a
    floating window
    
    Change-Id: Ie54545a4dc423484b6106a540f106467c25d8c49
    Signed-off-by: Evan Anderson <evan1124@gmail.com>
    Signed-off-by: scott <scott@ubuntu.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3094,6 +3094,13 @@ public final class Settings {
          */
         public static final String QS_COLLAPSE_PANEL = "qs_collapse_panel";
 
+	/**
+         * Quick Settings Launch in Floating Window
+         * 
+         * @hide
+         */
+        public static final String QS_FLOATING_WINDOW = "qs_floating_window";
+
         /**
          * Use the Notification Power Widget? (Who wouldn't!)
          *
