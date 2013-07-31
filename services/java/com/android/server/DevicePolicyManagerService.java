e9169b876c656387682f5da00a7d7175c33a2950
scotthartbti <scott@ubuntu.com>
2013-07-31 18:58:33 -0400

    Revert "Move seinfo and install-time changes to inside PMS."
    This reverts commit 869394e735bd9caac53c66681b495a6cec89629a.

--- services/java/com/android/server/DevicePolicyManagerService.java
+++ services/java/com/android/server/DevicePolicyManagerService.java
@@ -45,6 +45,7 @@ import android.content.pm.IPackageManager;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
+import android.content.pm.SELinuxMMAC;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -257,8 +258,6 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
             return true;
         }
     }
-
-    private static final String MMAC_ENFORCE_PROPERTY = "persist.mmac.enforce";
 
     private static final String SEPOLICY_PATH_SEPOLICY = "/data/security/sepolicy";
 
@@ -3046,12 +3045,12 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
             return false;
         }
 
-        boolean systemState = SystemProperties.getBoolean(MMAC_ENFORCE_PROPERTY, false);
+        boolean systemState = SELinuxMMAC.getEnforcingMode();
         boolean enforceMMAC = mmacAdmin.enforceMMAC;
         if (!firstBoot || !systemState) {
             if (systemState != enforceMMAC) {
                 Slog.v(TAG, "Changed MMAC enforcing status " + systemState + " to " + enforceMMAC);
-                SystemProperties.set(MMAC_ENFORCE_PROPERTY, enforceMMAC ? "true" : "false");
+                SELinuxMMAC.setEnforcingMode(enforceMMAC);
             }
         }
 
--- services/tests/servicestests/apks/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-FrameworkServicesTests_BUILD_PACKAGE := $(LOCAL_PATH)/FrameworkServicesTests_apk.mk
-
-# build sub packages
-include $(call all-makefiles-under,$(LOCAL_PATH))
