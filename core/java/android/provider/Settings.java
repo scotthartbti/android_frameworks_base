eea7d28db59a48b3d484782e7e1390e71e3dc3dd
Danny Baumann <dannybaumann@web.de>
2013-07-31 15:49:13 +0200

    Re-add auto brightness configuration UI (1/2)
    - Add a button into the new brightness dialog that allows opening the
      auto brightness setup
    - Enable auto-brightness adjustment (setting for general brighter/darker
      adjustment) and twilight adjustment
    
    Change-Id: I7fd11ddaa112396dcb7508692aacb93bfe3da256
    Signed-off-by: scott <scott@ubuntu.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/quicksettings/BrightnessTile.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1857,6 +1857,13 @@ public final class Settings {
          */
         public static final String AUTO_BRIGHTNESS_RESPONSIVENESS = "auto_brightness_responsiveness";
 
+	/**
+         * Whether to enable adjustment of automatic brightness adjustment
+         * to sunrise and sunset.
+         * @hide
+         */
+        public static final String AUTO_BRIGHTNESS_TWILIGHT_ADJUSTMENT = "auto_brightness_twilight_adjustment";
+
         /**
          * Touch Key Light Duration
          *
