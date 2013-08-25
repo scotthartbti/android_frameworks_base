f96e178630974da869f2900dc325545c8ceae351
Lars Greiss <kufikugel@googlemail.com>
2013-08-26 00:14:30 +0200

    Frameworks: simplify custom lockscreen background code
    - remove observer for transparency and handle it better
    - add multisuser handles
    - smaller fixes and code cleanup
    - simpfliy custom background handling and change to our needs (thanks Danish)
    
    Change-Id: Idf0bbb7bf9e132a3a68d567e3863cce1b6d99742
    Signed-off-by: scott <scott@ubuntu.com>
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -38,8 +38,11 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.RemoteControlClient;
 import android.os.Looper;
 import android.os.Parcel;
@@ -176,6 +179,7 @@ public class KeyguardHostView extends KeyguardViewBase {
             mCameraDisabled = dpm.getCameraDisabled(null);
         }
 
+        cleanupAppWidgetIds();
         mSafeModeEnabled = LockPatternUtils.isSafeModeEnabled();
         mUserSetupCompleted = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.USER_SETUP_COMPLETE, 0, UserHandle.USER_CURRENT) != 0;
@@ -195,8 +199,6 @@ public class KeyguardHostView extends KeyguardViewBase {
 
         mAppWidgetHost = new AppWidgetHost(userContext, APPWIDGET_HOST_ID, mOnClickHandler,
                 Looper.myLooper());
-
-        cleanupAppWidgetIds();
 
         mAppWidgetManager = AppWidgetManager.getInstance(userContext);
 
@@ -464,21 +466,30 @@ public class KeyguardHostView extends KeyguardViewBase {
             return;
         }
 
+        Drawable back = null;
+        int bgAlpha = (int)((1 - (Settings.System.getFloatForUser(getContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_ALPHA, 0.0f, UserHandle.USER_CURRENT))) * 255);
+
         if (!background.isEmpty()) {
             try {
-                setBackgroundColor(Integer.parseInt(background));
+                back = new ColorDrawable(Integer.parseInt(background));
             } catch(NumberFormatException e) {
                 Log.e(TAG, "Invalid background color " + background);
             }
         } else {
             try {
-                Context settingsContext = getContext().createPackageContext("com.android.settings", 0);
+               Context settingsContext = getContext().createPackageContext("com.android.settings", 0);
                 String wallpaperFile = settingsContext.getFilesDir() + "/lockwallpaper";
                 Bitmap backgroundBitmap = BitmapFactory.decodeFile(wallpaperFile);
-                setBackgroundDrawable(new BitmapDrawable(backgroundBitmap));
+                back = new BitmapDrawable(getContext().getResources(), backgroundBitmap);
             } catch (NameNotFoundException e) {
-            // Do nothing here
+                // Do nothing here
             }
+        }
+        if (back != null) {
+            back.setColorFilter(BACKGROUND_COLOR, PorterDuff.Mode.SRC_ATOP);
+            setBackground(back);
+            getBackground().setAlpha(bgAlpha);
         }
     }
 
