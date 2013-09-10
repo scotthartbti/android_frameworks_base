e9fcf71c74a1aa526ea29bea92cefb86946a017a
Aleksander Ciepiela <alekciepiela@gmail.com>
2013-09-10 19:53:54 +0200

    SystemUI/Added ADB over network QuickSettings Tile
    Change-Id: I340846a171571a7b61d2c68f2a6875794be5d5b2
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- core/java/com/android/internal/util/beanstalk/DeviceUtils.java
+++ core/java/com/android/internal/util/beanstalk/DeviceUtils.java
@@ -2,6 +2,7 @@
 package com.android.internal.util.beanstalk;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -13,6 +14,7 @@ import android.hardware.display.DisplayManager;
 import android.hardware.display.WifiDisplayStatus;
 import android.net.ConnectivityManager;
 import android.nfc.NfcAdapter;
+import android.provider.Settings;
 import android.os.Vibrator;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
@@ -79,6 +81,10 @@ public class DeviceUtils {
     public static boolean deviceSupportsImeSwitcher(Context ctx) {
         Resources res = ctx.getResources();
         return res.getBoolean(com.android.internal.R.bool.config_show_cmIMESwitcher);
+    }
+
+    public static boolean adbEnabled(ContentResolver resolver) {
+            return (Settings.Global.getInt(resolver, Settings.Global.ADB_ENABLED, 0)) == 1;
     }
 
     public static boolean deviceSupportsVibrator(Context ctx) {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -40,6 +40,7 @@ import static com.android.internal.util.beanstalk.QSConstants.TILE_LTE;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_MOBILEDATA;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_MUSIC;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_NETWORKMODE;
+import static com.android.internal.util.beanstalk.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_NFC;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_QUICKRECORD;
 import static com.android.internal.util.beanstalk.QSConstants.TILE_QUIETHOURS;
@@ -102,6 +103,7 @@ import com.android.systemui.quicksettings.MobileNetworkTile;
 import com.android.systemui.quicksettings.MobileNetworkTypeTile;
 import com.android.systemui.quicksettings.MusicTile;
 import com.android.systemui.quicksettings.InternalMemory;
+import com.android.systemui.quicksettings.NetworkAdbTile;
 import com.android.systemui.quicksettings.NfcTile;
 import com.android.systemui.quicksettings.PreferencesTile;
 import com.android.systemui.quicksettings.QuickSettingsTile;
@@ -288,6 +290,11 @@ public class QuickSettingsController {
                 qs = new OnTheGoTile(mContext, this);
             } else if (tile.equals(TILE_BATTERYSAVER)) {
                 qs = new BatterySaverTile(mContext, this);
+            } else if (tile.equals(TILE_NETWORKADB)) {
+                mTileStatusUris.add(Settings.Global.getUriFor(Settings.Global.ADB_ENABLED));
+                if (DeviceUtils.adbEnabled(resolver)) {
+                    qs = new NetworkAdbTile(mContext, this);
+                }
             }
 
             if (qs != null) {
