0df78ce8ec46fc035f71aa611d1ecfbcd1d6477b
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    [1/2] SystemUI: Network speed indicator
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3082,6 +3082,11 @@ public final class Settings {
          */
         public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
 
+	    /** Network speed indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
