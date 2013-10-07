9a0e0a58e5364af9e3bd844bee9be1dc00cc0330
scott <scott@ubuntu.com>
2013-10-06 23:56:30 -0400

    Revert "Active display: Catch Resources.NotFoundException"
    This reverts commit d02b8944c02edcfda010adb038f201a03c1d6a00.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -855,7 +855,6 @@ public class ActiveDisplayView extends FrameLayout {
                     }
                 } catch (RemoteException re) {
                 } catch (NameNotFoundException nnfe) {
-                } catch (Resources.NotFoundException e) {
                 }
             }
         });
@@ -973,7 +972,6 @@ public class ActiveDisplayView extends FrameLayout {
                 }
             });
         } catch (NameNotFoundException e) {
-        } catch (Resources.NotFoundException e) {
         }
     }
 
