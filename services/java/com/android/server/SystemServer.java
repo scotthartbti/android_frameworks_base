fc4b1f53aaecc303b494b5f7f21ff374858d81f5
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-11-14 23:07:12 +0200

    framework/base: Add EdgeGesture service.
    Based on code of PieService.
    
    PS
    - add more sensitive steps
    
    default: 5, minimum: 1, maximum: 10
    PS
    - fix mask derp
    PS
    - cleanup
    - reduce left and right trigger to from 80% to 70%
      till new size flage for the service are ready
    
    Change-Id: I053737e36340a5ec6814b21eb87575f9689c7e88
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	Android.mk
    	core/res/res/values/dimens.xml
    	core/res/res/values/slim_symbols.xml
    	services/java/com/android/server/SystemServer.java
    	services/java/com/android/server/input/InputManagerService.java

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -65,6 +65,7 @@ import com.android.server.media.MediaRouterService;
 import com.android.server.net.NetworkPolicyManagerService;
 import com.android.server.net.NetworkStatsService;
 import com.android.server.os.SchedulingPolicyService;
+import com.android.server.gesture.EdgeGestureService;
 import com.android.server.pm.Installer;
 import com.android.server.pm.PackageManagerService;
 import com.android.server.pm.UserManagerService;
@@ -383,6 +384,7 @@ class ServerThread {
         PrintManagerService printManager = null;
         GestureService gestureService = null;
         MediaRouterService mediaRouter = null;
+        EdgeGestureService edgeGestureService = null;
 
         // Bring up services needed for UI.
         if (factoryTest != SystemServer.FACTORY_TEST_LOW_LEVEL) {
@@ -877,6 +879,14 @@ class ServerThread {
                     reportWtf("starting MediaRouterService", e);
                 }
             }
+
+            try {
+                Slog.i(TAG, "EdgeGesture service");
+                edgeGestureService = new EdgeGestureService(context, inputManager);
+                ServiceManager.addService("edgegestureservice", edgeGestureService);
+            } catch (Throwable e) {
+                Slog.e(TAG, "Failure starting EdgeGesture service", e);
+            }
         }
 
         // make sure the ADB_ENABLED setting value matches the secure property value
@@ -987,6 +997,14 @@ class ServerThread {
         filter.addCategory(Intent.CATEGORY_THEME_PACKAGE_INSTALLED_STATE_CHANGE);
         filter.addDataScheme("package");
         context.registerReceiver(new AppsLaunchFailureReceiver(), filter);
+
+        if (edgeGestureService != null) {
+            try {
+                edgeGestureService.systemReady();
+            } catch (Throwable e) {
+                reportWtf("making EdgeGesture service ready", e);
+            }
+        }
 
         // These are needed to propagate to the runnable below.
         final Context contextF = context;
