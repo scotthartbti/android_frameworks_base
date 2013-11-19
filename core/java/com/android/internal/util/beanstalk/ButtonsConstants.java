2c72b9a61ea431b9723e8fa61de230cacea6cebc
Scott <scott@ubuntu>
2013-11-18 23:57:18 -0500

    fb: add back notification shortcuts feature + common slim classes
        beside our unique slim feature this patch adds
    
        - all system shortcut icons we need for this and other stuff in future
        - basice slim helper and constants classes we need later as well
        - device utils helper
        - preparation for navbar navring and other shortcut features
        - and of course our notification shortcuts feature
    
    Change-Id: Ic2f3366c85d2cb9be324b762e3510c7630caad24
    Signed-off-by: Scott <scott@ubuntu>

---
+++ core/java/com/android/internal/util/beanstalk/ButtonsConstants.java
@@ -0,0 +1,20 @@
+
+package com.android.internal.util.beanstalk;
+
+public class ButtonsConstants {
+
+    public static final String ACTION_TORCH                = "**torch**";
+
+    // no action
+    public static final String ACTION_NULL            = "**null**";
+
+    // this shorcut constant is only used to identify if the user
+    // selected in settings a custom app...after it is choosed intent uri
+    // is saved in the ButtonConfig object
+    public static final String ACTION_APP          = "**app**";
+
+    public static final String ICON_EMPTY = "empty";
+    public static final String SYSTEM_ICON_IDENTIFIER = "system_shortcut=";
+    public static final String ACTION_DELIMITER = "|";
+
+}
