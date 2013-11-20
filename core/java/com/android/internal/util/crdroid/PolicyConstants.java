SlimRoms/frameworks_base

Frameworks: Advanced Power Menu
Change-Id: I1c9af874a340e4fccbef09a5879f0b718d6998dd

core/java/com/android/internal/util/slim/PolicyConstants.java
@@ -1,0 +1,34 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.slim;
+
+public class PolicyConstants {
+
+    public static final String ACTION_POWER_OFF  = "**poweroff**";
+    public static final String ACTION_REBOOT     = "**reboot**";
+    public static final String ACTION_SCREENSHOT = "**screenshot**";
+    public static final String ACTION_AIRPLANE   = "**airplane**";
+    public static final String ACTION_SOUND      = "**sound**";
+
+    public static final String ACTION_DELIMITER  = "|";
+
+    public static final String POWER_MENU_CONFIG_DEFAULT =
+          ACTION_POWER_OFF  + ACTION_DELIMITER
+        + ACTION_REBOOT     + ACTION_DELIMITER
+        + ACTION_AIRPLANE   + ACTION_DELIMITER
+        + ACTION_SOUND;
+}

core/java/com/android/internal/util/slim/PolicyHelper.java
@@ -1,0 +1,60 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.slim;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import java.util.ArrayList;
+
+public class PolicyHelper {
+
+    private PolicyHelper() {
+    }
+
+    public static String[] getPowerMenuConfig(Context context) {
+        return (getPowerMenuValues(context, getPowerMenuProvider(context)));
+    }
+
+    private static String getPowerMenuProvider(Context context) {
+        String config = Settings.System.getStringForUser(
+                    context.getContentResolver(),
+                    Settings.System.POWER_MENU_CONFIG,
+                    UserHandle.USER_CURRENT);
+        if (config == null) {
+            config = PolicyConstants.POWER_MENU_CONFIG_DEFAULT;
+        }
+        return config;
+    }
+
+    public static void setNavBarConfig(Context context,
+            ArrayList<String> powerMenuConfig, boolean reset) {
+        String config;
+        if (reset) {
+            config = PolicyConstants.POWER_MENU_CONFIG_DEFAULT;
+        } else {
+            config = getPowerMenuProvider(context);
+        }
+        Settings.System.putString(context.getContentResolver(),
+                    Settings.System.POWER_MENU_CONFIG, config);
+    }
+
+    public static String[] getPowerMenuValues(Context context, String config) {
+        return config.split("\\" + PolicyConstants.ACTION_DELIMITER);
+    }
+}

scotthartbti/android_frameworks_base

Conflicts:
	core/res/res/values/slim_strings.xml
	core/res/res/values/slim_symbols.xml
	packages/SystemUI/res/layout/notification_shortcut_item.xml
	policy/src/com/android/internal/policy/impl/GlobalActions.java
	services/java/com/android/server/power/ShutdownThread.java
	services/java/com/android/server/wm/WindowManagerService.java

core/java/com/android/internal/util/crdroid/PolicyConstants.java
@@ -1,0 +1,34 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.crdroid;
+
+public class PolicyConstants {
+
+    public static final String ACTION_POWER_OFF  = "**poweroff**";
+    public static final String ACTION_REBOOT     = "**reboot**";
+    public static final String ACTION_SCREENSHOT = "**screenshot**";
+    public static final String ACTION_AIRPLANE   = "**airplane**";
+    public static final String ACTION_SOUND      = "**sound**";
+
+    public static final String ACTION_DELIMITER  = "|";
+
+    public static final String POWER_MENU_CONFIG_DEFAULT =
+          ACTION_POWER_OFF  + ACTION_DELIMITER
+        + ACTION_REBOOT     + ACTION_DELIMITER
+        + ACTION_AIRPLANE   + ACTION_DELIMITER
+        + ACTION_SOUND;
+}

core/java/com/android/internal/util/crdroid/PolicyHelper.java
@@ -1,0 +1,60 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.crdroid;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import java.util.ArrayList;
+
+public class PolicyHelper {
+
+    private PolicyHelper() {
+    }
+
+    public static String[] getPowerMenuConfig(Context context) {
+        return (getPowerMenuValues(context, getPowerMenuProvider(context)));
+    }
+
+    private static String getPowerMenuProvider(Context context) {
+        String config = Settings.System.getStringForUser(
+                    context.getContentResolver(),
+                    Settings.System.POWER_MENU_CONFIG,
+                    UserHandle.USER_CURRENT);
+        if (config == null) {
+            config = PolicyConstants.POWER_MENU_CONFIG_DEFAULT;
+        }
+        return config;
+    }
+
+    public static void setNavBarConfig(Context context,
+            ArrayList<String> powerMenuConfig, boolean reset) {
+        String config;
+        if (reset) {
+            config = PolicyConstants.POWER_MENU_CONFIG_DEFAULT;
+        } else {
+            config = getPowerMenuProvider(context);
+        }
+        Settings.System.putString(context.getContentResolver(),
+                    Settings.System.POWER_MENU_CONFIG, config);
+    }
+
+    public static String[] getPowerMenuValues(Context context, String config) {
+        return config.split("\\" + PolicyConstants.ACTION_DELIMITER);
+    }
+}
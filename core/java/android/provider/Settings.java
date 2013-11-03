160676eaecf1985a771982e91a9380ccfdd2677e
Griffin Millender <griffinn.millender@gmail.com>
2013-11-02 19:18:29 -0500

    Frameworks: Advanced Power Menu
    Change-Id: I1c9af874a340e4fccbef09a5879f0b718d6998dd
    
    Conflicts:
    	core/res/res/values/slim_strings.xml
    	core/res/res/values/slim_symbols.xml
    	packages/SystemUI/res/layout/notification_shortcut_item.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    	services/java/com/android/server/power/ShutdownThread.java
    	services/java/com/android/server/wm/WindowManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3588,6 +3588,13 @@ public final class Settings {
         public static final String NOTIFICATION_SHORTCUTS_COLOR_MODE = "notification_shortcuts_color_mode";
 
         /**
+         * Config for advanced power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_CONFIG = "power_menu_config";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
