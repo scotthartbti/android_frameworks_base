d31d2606e127176703ba3ffd59f698ecfafc91d7
Lars Greiss <kufikugel@googlemail.com>
2013-11-23 02:44:41 +0100

    fb: system wide immersive mode aka expanded desktop thanks to
    Danny and Kiril for the base
    
    and kroz for the graphics
    
    At least it is expanded desktop mode like on 4.3 and before but using
    not the nice new immersive mode from google which was introduced with 4.4
    Means everwhere it is swipable in from the top or bottom depends on
    if the navbar or statusbar is existend. As well it respects our other
    features like 0dp settings for navbar in landscape or portrait etc.
    The option to choose the mode what the user want to hide we removed
    due that it is now absolutly not needed. 1. it is swipeable now
    2. The user can disable navbar like he want in navbar settings if
    urgently needed
    
    As well added shortcut support for navbar navring and powermenu
    
    Change-Id: I37e8f9571cf622154d347b7eabd13bffc99ae187
    Signed-off-by: Scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/slim_symbols.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3682,6 +3682,12 @@ public final class Settings {
         public static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
 
 	/**
+         * Expanded desktop on/off state
+         * @hide
+         */
+        public static final String EXPANDED_DESKTOP_STATE = "expanded_desktop_state";
+
+	/**
          * whether which Ram Usage Bar mode is used on recent switcher
          * 0 = none, 1 = only app use, 2 = app and cache use, 3 = app, cache and system use
          * @hide
