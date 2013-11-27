thefutureosp/android_frameworks_base-1

Frameworks: Port Lockscreen Targets and Shortcuts
    First thanks to PA and CM which gave us partly code and some ideas.
    Still some parts of the original implementation are/were buggy as well
    we needed to ehance it.

    Whatever the new Slim version is now ready and seems to be completly bug
    free.

    - Configurable lockscreen targets...on tablets 8 on phone either 5 or 8
    - configurable lockscreen shortcuts only on phones...up to 5 shortcuts
    - Centralazing some methods....using now then rounded corner on custom
      images systemwide...means on all other shorcut features we have
    - multiDpi compatible
    - new settings logic
    - instance handling of all dialogs like we do on all other places
      way nicer this way
    - tons of fixes

Change-Id: Ie62857beeacceb880efe9aa57246e111d64cd00e

core/java/com/android/internal/util/crdroid/ImageHelper.java
@@ -1,0 +1,104 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.crdroid;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+
+public class ImageHelper {
+
+    public static Bitmap getColoredBitmap(Drawable d, int color) {
+        if (d == null) {
+            return null;
+        }
+        Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
+        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
+        Paint pp = new Paint();
+        PorterDuffColorFilter frontFilter =
+            new PorterDuffColorFilter(color, Mode.MULTIPLY);
+        pp.setColorFilter(frontFilter);
+        Canvas cc = new Canvas(grayscaleBitmap);
+        cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
+        return grayscaleBitmap;
+    }
+
+    private static Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+    public static Drawable resize(Context context, Drawable image, int size) {
+        if (image == null || context == null) {
+            return null;
+        }
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size,
+                context.getResources().getDisplayMetrics());
+
+        Bitmap d = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, px, px, false);
+        return new BitmapDrawable(context.getResources(), bitmapOrig);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
+                Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 24;
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -58,7 +58,7 @@ import android.widget.FrameLayout;
 import com.android.internal.util.crdroid.ButtonConfig;
 import com.android.internal.util.crdroid.ButtonsConstants;
 import com.android.internal.util.crdroid.ButtonsHelper;
-import com.android.internal.util.crdroid.ColorHelper;
+import com.android.internal.util.crdroid.ImageHelper;
 import com.android.internal.util.crdroid.DeviceUtils;
 import com.android.internal.util.crdroid.SlimActions;
 import com.android.internal.widget.multiwaveview.GlowPadView;
@@ -480,11 +480,14 @@ public class SearchPanelView extends FrameLayout implements
                 try {
                     Drawable customIcon;
                     if (iconFile.exists()) {
-                        customIcon = ColorHelper.resize(mContext,
-                            new BitmapDrawable(getResources(), iconFile.getAbsolutePath()), 50);
+                        customIcon = new BitmapDrawable(getResources(),
+                                        ImageHelper.getRoundedCornerBitmap(
+                                        new BitmapDrawable(getResources(),
+                                        iconFile.getAbsolutePath()).getBitmap()));
+                        customIcon = ImageHelper.resize(mContext, customIcon, 50);
                     } else {
                         customIcon = new BitmapDrawable(getResources(),
-                                    ColorHelper.getColoredBitmap(ColorHelper.resize(mContext,
+                                    ImageHelper.getColoredBitmap(ImageHelper.resize(mContext,
                                     getResources().getDrawable(getResources().getIdentifier(
                                     customIconUri.substring(
                                     ButtonsConstants.SYSTEM_ICON_IDENTIFIER.length()),
@@ -544,7 +547,7 @@ public class SearchPanelView extends FrameLayout implements
 
         if (aInfo != null && pm != null) {
             return new TargetDrawable(mResources,
-                setStateListDrawable(ColorHelper.resize(mContext, aInfo.loadIcon(pm), 50)));
+                setStateListDrawable(ImageHelper.resize(mContext, aInfo.loadIcon(pm), 50)));
         }
         return noneDrawable;
     }
@@ -553,9 +556,9 @@ public class SearchPanelView extends FrameLayout implements
         if (activityIcon == null) {
             return null;
         }
-        Drawable iconBg = ColorHelper.resize(mContext,
+        Drawable iconBg = ImageHelper.resize(mContext,
             mResources.getDrawable(R.drawable.ic_navbar_blank), 50);
-        Drawable iconBgActivated = ColorHelper.resize(mContext,
+        Drawable iconBgActivated = ImageHelper.resize(mContext,
             mResources.getDrawable(R.drawable.ic_navbar_blank_activated), 50);
         int margin = (int)(iconBg.getIntrinsicHeight() / 3);
         LayerDrawable icon = new LayerDrawable (new Drawable[] {iconBg, activityIcon});

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -56,8 +56,9 @@ import android.widget.LinearLayout;
 import com.android.internal.util.crdroid.ButtonConfig;
 import com.android.internal.util.crdroid.ButtonsConstants;
 import com.android.internal.util.crdroid.ButtonsHelper;
-import com.android.internal.util.crdroid.ColorHelper;
+import com.android.internal.util.crdroid.ImageHelper;
 import com.android.internal.util.crdroid.DeviceUtils;
+
 import com.android.systemui.R;
 import com.android.systemui.statusbar.BaseStatusBar;
 import com.android.systemui.statusbar.DelegateViewHelper;
@@ -393,7 +394,7 @@ public class NavigationBarView extends LinearLayout {
         mBackAltIcon = res.getDrawable(R.drawable.ic_sysbar_back_ime);
         if (mNavBarButtonColorMode != 3) {
             mBackAltIcon = new BitmapDrawable(mContext.getResources(),
-                ColorHelper.getColoredBitmap(mBackAltIcon, mNavBarButtonColor));
+                ImageHelper.getColoredBitmap(mBackAltIcon, mNavBarButtonColor));
         }
 
         // now the keyguard searchlight and camera button
@@ -406,7 +407,7 @@ public class NavigationBarView extends LinearLayout {
         if (searchLight != null && defaultSearchLightDrawable != null) {
             if (mNavBarButtonColorMode != 3) {
                 searchLight.setImageBitmap(
-                    ColorHelper.getColoredBitmap(defaultSearchLightDrawable, mNavBarButtonColor));
+                    ImageHelper.getColoredBitmap(defaultSearchLightDrawable, mNavBarButtonColor));
             } else {
                 searchLight.setImageDrawable(defaultSearchLightDrawable);
             }
@@ -414,7 +415,7 @@ public class NavigationBarView extends LinearLayout {
         if (cameraButton != null && defaultCameraButtonDrawable != null) {
             if (mNavBarButtonColorMode != 3) {
                 cameraButton.setImageBitmap(
-                    ColorHelper.getColoredBitmap(defaultCameraButtonDrawable, mNavBarButtonColor));
+                    ImageHelper.getColoredBitmap(defaultCameraButtonDrawable, mNavBarButtonColor));
             } else {
                 cameraButton.setImageDrawable(defaultCameraButtonDrawable);
             }
@@ -469,7 +470,7 @@ public class NavigationBarView extends LinearLayout {
         Drawable d = ButtonsHelper.getButtonIconImage(mContext, clickAction, iconUri);
         if (d != null) {
             if (colorize && mNavBarButtonColorMode != 3) {
-                v.setImageBitmap(ColorHelper.getColoredBitmap(d, mNavBarButtonColor));
+                v.setImageBitmap(ImageHelper.getColoredBitmap(d, mNavBarButtonColor));
             } else {
                 v.setImageDrawable(d);
             }
@@ -520,7 +521,7 @@ public class NavigationBarView extends LinearLayout {
 
         Drawable d = mContext.getResources().getDrawable(R.drawable.ic_sysbar_menu);
         if (mNavBarButtonColorMode != 3) {
-            v.setImageBitmap(ColorHelper.getColoredBitmap(d, mNavBarButtonColor));
+            v.setImageBitmap(ImageHelper.getColoredBitmap(d, mNavBarButtonColor));
         } else {
             v.setImageDrawable(d);
         }

scotthartbti/android_frameworks_base

Signed-off-by: Scott <scott@ubuntu>
Conflicts:
	core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
	packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
	packages/SystemUI/src/com/android/systemui/SearchPanelView.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/ShortcutsWidget.java
	policy/src/com/android/internal/policy/impl/GlobalActions.java

core/java/com/android/internal/util/beanstalk/ImageHelper.java
@@ -1,0 +1,104 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.beanstalk;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+
+public class ImageHelper {
+
+    public static Bitmap getColoredBitmap(Drawable d, int color) {
+        if (d == null) {
+            return null;
+        }
+        Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
+        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
+        Paint pp = new Paint();
+        PorterDuffColorFilter frontFilter =
+            new PorterDuffColorFilter(color, Mode.MULTIPLY);
+        pp.setColorFilter(frontFilter);
+        Canvas cc = new Canvas(grayscaleBitmap);
+        cc.drawBitmap(grayscaleBitmap, 0, 0, pp);
+        return grayscaleBitmap;
+    }
+
+    private static Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+    public static Drawable resize(Context context, Drawable image, int size) {
+        if (image == null || context == null) {
+            return null;
+        }
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size,
+                context.getResources().getDisplayMetrics());
+
+        Bitmap d = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, px, px, false);
+        return new BitmapDrawable(context.getResources(), bitmapOrig);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
+                Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 24;
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -58,7 +58,7 @@ import android.widget.FrameLayout;
 import com.android.internal.util.beanstalk.ButtonConfig;
 import com.android.internal.util.beanstalk.ButtonsConstants;
 import com.android.internal.util.beanstalk.ButtonsHelper;
-import com.android.internal.util.beanstalk.ColorHelper;
+import com.android.internal.util.beanstalk.ImageHelper;
 import com.android.internal.util.beanstalk.DeviceUtils;
 import com.android.internal.util.beanstalk.SlimActions;
 import com.android.internal.widget.multiwaveview.GlowPadView;
@@ -480,11 +480,14 @@ public class SearchPanelView extends FrameLayout implements
                 try {
                     Drawable customIcon;
                     if (iconFile.exists()) {
-                        customIcon = ColorHelper.resize(mContext,
-                            new BitmapDrawable(getResources(), iconFile.getAbsolutePath()), 50);
+                        customIcon = new BitmapDrawable(getResources(),
+                                        ImageHelper.getRoundedCornerBitmap(
+                                        new BitmapDrawable(getResources(),
+                                        iconFile.getAbsolutePath()).getBitmap()));
+                        customIcon = ImageHelper.resize(mContext, customIcon, 50);
                     } else {
                         customIcon = new BitmapDrawable(getResources(),
-                                    ColorHelper.getColoredBitmap(ColorHelper.resize(mContext,
+                                    ImageHelper.getColoredBitmap(ImageHelper.resize(mContext,
                                     getResources().getDrawable(getResources().getIdentifier(
                                     customIconUri.substring(
                                     ButtonsConstants.SYSTEM_ICON_IDENTIFIER.length()),
@@ -544,7 +547,7 @@ public class SearchPanelView extends FrameLayout implements
 
         if (aInfo != null && pm != null) {
             return new TargetDrawable(mResources,
-                setStateListDrawable(ColorHelper.resize(mContext, aInfo.loadIcon(pm), 50)));
+                setStateListDrawable(ImageHelper.resize(mContext, aInfo.loadIcon(pm), 50)));
         }
         return noneDrawable;
     }
@@ -553,9 +556,9 @@ public class SearchPanelView extends FrameLayout implements
         if (activityIcon == null) {
             return null;
         }
-        Drawable iconBg = ColorHelper.resize(mContext,
+        Drawable iconBg = ImageHelper.resize(mContext,
             mResources.getDrawable(R.drawable.ic_navbar_blank), 50);
-        Drawable iconBgActivated = ColorHelper.resize(mContext,
+        Drawable iconBgActivated = ImageHelper.resize(mContext,
             mResources.getDrawable(R.drawable.ic_navbar_blank_activated), 50);
         int margin = (int)(iconBg.getIntrinsicHeight() / 3);
         LayerDrawable icon = new LayerDrawable (new Drawable[] {iconBg, activityIcon});

packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -52,12 +52,12 @@ import android.view.accessibility.AccessibilityManager.TouchExplorationStateChan
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
 import com.android.internal.util.beanstalk.ButtonConfig;
 import com.android.internal.util.beanstalk.ButtonsConstants;
 import com.android.internal.util.beanstalk.ButtonsHelper;
-import com.android.internal.util.beanstalk.ColorHelper;
+import com.android.internal.util.beanstalk.ImageHelper;
 import com.android.internal.util.beanstalk.DeviceUtils;
+
 import com.android.systemui.R;
 import com.android.systemui.statusbar.BaseStatusBar;
 import com.android.systemui.statusbar.DelegateViewHelper;
@@ -393,7 +393,7 @@ public class NavigationBarView extends LinearLayout {
         mBackAltIcon = res.getDrawable(R.drawable.ic_sysbar_back_ime);
         if (mNavBarButtonColorMode != 3) {
             mBackAltIcon = new BitmapDrawable(mContext.getResources(),
-                ColorHelper.getColoredBitmap(mBackAltIcon, mNavBarButtonColor));
+                ImageHelper.getColoredBitmap(mBackAltIcon, mNavBarButtonColor));
         }
 
         // now the keyguard searchlight and camera button
@@ -406,7 +406,7 @@ public class NavigationBarView extends LinearLayout {
         if (searchLight != null && defaultSearchLightDrawable != null) {
             if (mNavBarButtonColorMode != 3) {
                 searchLight.setImageBitmap(
-                    ColorHelper.getColoredBitmap(defaultSearchLightDrawable, mNavBarButtonColor));
+                    ImageHelper.getColoredBitmap(defaultSearchLightDrawable, mNavBarButtonColor));
             } else {
                 searchLight.setImageDrawable(defaultSearchLightDrawable);
             }
@@ -414,7 +414,7 @@ public class NavigationBarView extends LinearLayout {
         if (cameraButton != null && defaultCameraButtonDrawable != null) {
             if (mNavBarButtonColorMode != 3) {
                 cameraButton.setImageBitmap(
-                    ColorHelper.getColoredBitmap(defaultCameraButtonDrawable, mNavBarButtonColor));
+                    ImageHelper.getColoredBitmap(defaultCameraButtonDrawable, mNavBarButtonColor));
             } else {
                 cameraButton.setImageDrawable(defaultCameraButtonDrawable);
             }
@@ -469,7 +469,7 @@ public class NavigationBarView extends LinearLayout {
         Drawable d = ButtonsHelper.getButtonIconImage(mContext, clickAction, iconUri);
         if (d != null) {
             if (colorize && mNavBarButtonColorMode != 3) {
-                v.setImageBitmap(ColorHelper.getColoredBitmap(d, mNavBarButtonColor));
+                v.setImageBitmap(ImageHelper.getColoredBitmap(d, mNavBarButtonColor));
             } else {
                 v.setImageDrawable(d);
             }
@@ -520,7 +520,7 @@ public class NavigationBarView extends LinearLayout {
 
         Drawable d = mContext.getResources().getDrawable(R.drawable.ic_sysbar_menu);
         if (mNavBarButtonColorMode != 3) {
-            v.setImageBitmap(ColorHelper.getColoredBitmap(d, mNavBarButtonColor));
+            v.setImageBitmap(ImageHelper.getColoredBitmap(d, mNavBarButtonColor));
         } else {
             v.setImageDrawable(d);
         }