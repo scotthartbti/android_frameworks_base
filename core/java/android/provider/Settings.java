b4cc926a6dd854e40b9f4e37069ee3c4f0349a8e
scott <scott@ubuntu>
2013-12-08 18:21:12 -0500

    add screen record to power menu
    Change-Id: Ifb65938e19c730b63777b9182f1ab6bdf5cb77ae
    Signed-off-by: scott <scott@ubuntu>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3330,6 +3330,12 @@ public final class Settings {
          */
         public static final String POWER_MENU_SCREENSHOT_ENABLED = "power_menu_screenshot_enabled";
 
+	/**
+         * Show ScreenRecord in Power Menu
+         * @hide
+         */
+        public static final String SCREENRECORD_IN_POWER_MENU = "screenrecord_in_power_menu";
+
         /**
          * Whether power menu profiles switcher is enabled
          * @hide
