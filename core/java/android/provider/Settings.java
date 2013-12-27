b05e1c50764445336e1392aa9aa691dfd9f5e28a
Jon Haus <haus.xda@gmail.com>
2013-12-27 06:41:24 -0500

    [1/2] SystemUI: Update network traffic indicator
    Android measures the amount of data transferred in bytes,
    however, by convention, data rates should be measured in bits
    per second.
    
    *Convert the change in total data and time to bits and seconds,
    and then divide the amount transferred by the number of
    seconds that have elapsed.
    
    *Update the labels and have them use the correct SI prefixes.
    
    *Factor out or add @hide to public methods and constructors
    
    *Remove all Runnables and Messages from Handler queue when
    disabled
    
    *Minor formatting changes to stay consistent with Android's
    code-style guidelines.
    https://source.android.com/source/code-style.html
    
    PS2: Set maximum digits to prevent it from filling the statusbar
    PS3: Rebase
    PS4: Add ability to display either bits or bytes
    
    Change-Id: I9add610a8d14f5a48f2af0a82ced0c186224e282
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3161,6 +3161,9 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /** Network speed indicator
+         * 0 = disabled
+         * 1 = show bits per second
+         * 2 = show Bytes per second
          * @hide
          */
         public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
