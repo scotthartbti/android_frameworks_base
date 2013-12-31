d02ca14a8eda8900cabcd62f5ac8666e71605717
hany <hany.alsamman@gmail.com>
2013-12-31 17:52:36 +0000

    prevent multiwindow if halo enabled please multiwindow stay out if HALO there !! , but we will leave you in activity
    Change-Id: Ie9ca36b6c7647ff9c6f038986b107f3623d8726c
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    
    	core/java/android/inputmethodservice/InputMethodService.java
    
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -914,11 +914,12 @@ public class InputMethodService extends AbstractInputMethodService {
         boolean fullScreenOverride = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.DISABLE_FULLSCREEN_KEYBOARD, 0,
                 UserHandle.USER_CURRENT_OR_SELF) != 0;
+        int mHaloEnabled = (Settings.System.getInt(getContentResolver(), Settings.System.HALO_ENABLED, 0));
         boolean isFullscreen;
         if (fullScreenOverride) {
             isFullscreen = false;
         } else {
-            isFullscreen = mShowInputRequested && onEvaluateFullscreenMode && onEvaluateSplitView() ;
+            isFullscreen = (mHaloEnabled != 1) ? (onEvaluateFullscreenMode() || onEvaluateSplitView()) : mShowInputRequested && onEvaluateFullscreenMode();
         }
         boolean changed = mLastShowInputRequested != mShowInputRequested;
         if (mIsFullscreen != isFullscreen || !mFullscreenApplied) {
