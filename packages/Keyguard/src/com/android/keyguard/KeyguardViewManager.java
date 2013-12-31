c7495776d2a0385ab5ab6cffd90b6e63969c6416
David L.-W <david.leppla.weber@gmail.com>
2013-12-31 14:17:47 +0100

    [1/2] FW: LockScreen Notifications by AOSPAL
    THANKS AOSPAL -> David L.-W <david.leppla.weber@gmail.com>
    
    Port from: http://review.carbon-rom.com/#/c/1399/
    
    My try on making notifications easily accessable from lockscreen.
    Credits to ChameleonOS, I got the notification listener permission and some small other stuff from their ActiveDisplay implementation.
    
    Lockscreen Notifications allow you to manage all your notifications directly on the lockscreen.
    
    Features:
    - Pocket mode turn screen on when phone is pulled out of pocket and turn it off when phone gets back in pocket
    - Hide low priority notifications
    - Hide non-clearable notifications
    - Allow dismissing even non-clearable notifications from lockscreen
    - Just turn screen on when there's currently a notification pending
    - Toggle between expanded and normal notification view by longpressing
    
    This should still be considered as beta, even if it seems to work very well for me.
    
    Lockscreen Notifications: Make "wake on notifications" user configurable
    
    and some other small changes that hopefully will improve battery live
    
    Lockscreen notifications: make height user configurable [1/2]
    
    Lockscreen Notifications: fix possible NPE
    
    Lockscreen Notifications: try on fixing notifications sometimes not responding
    
    Lockscreen Notifications: always scroll to bottom so one sees the newest notification first
    
    Lockscreen Notificaitons: Fix NPE while on setup wizard on clean flash
    
    Lockscreen Notifications: several fixes
    
    - fix notification not responding when removed and added in a short period of time (e.g. gravity screen start/stop)
    - just unlock screen on notification click when there's an activity to start
    - fix notification hide animation
    - just wake screen on reposted notifications when it's not ongoing (wake on notification before made the device stay awake for ever when a download/countdown or something similiar was running)
    
    PS3: Fixes & New Options (privacy, position)
    
    Lockscreen Notifications: just scroll down in "showAllNotifications"
    
    Lockscreen Notifications: Copyright
    
    Lockscreen Notifications: make offset top user configurable & add privacy mode [1/2]
    
    Lockscreen Notifications: Fix possible NPE
    
    Lockscreen Notifications: make width dynamic (WRAP_CONTENT)
    
    PS4: clean whitespace
    PS5: hope this fix the bug
    PS6: Off by default
    PS7: Fix logic
         Fix IllegalArgumentException
    
    Change-Id: Ibd73d05b5440c7376b87018ae7c702e3b85875b6
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardViewManager.java
@@ -115,6 +115,10 @@ public class KeyguardViewManager {
 
     private boolean mUnlockKeyDown = false;
 
+    private NotificationHostView mNotificationView;
+    private NotificationViewManager mNotificationViewManager;
+    private boolean mLockscreenNotifications = false;
+
     private KeyguardUpdateMonitorCallback mBackgroundChanger = new KeyguardUpdateMonitorCallback() {
         @Override
         public void onSetBackground(Bitmap bmp) {
@@ -142,6 +146,8 @@ public class KeyguardViewManager {
                     Settings.System.LOCKSCREEN_BLUR_BEHIND), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.LOCKSCREEN_BLUR_RADIUS), false, this);
+	    resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_NOTIFICATIONS), false, this);
         }
 
         @Override
@@ -149,12 +155,25 @@ public class KeyguardViewManager {
             synchronized (this) {
                 updateSettings();
                 setKeyguardParams();
-                mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
+                try {
+                	mViewManager.updateViewLayout(mKeyguardHost, mWindowLayoutParams);
+            	} catch(IllegalArgumentException e) {
+                	// Call yo mom call yo dad!
+            	}
             }
         }
     }
 
     private void updateSettings() {
+	mLockscreenNotifications = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_NOTIFICATIONS, 0) == 1;
+        if(mLockscreenNotifications && mNotificationViewManager == null) {
+            mNotificationViewManager = new NotificationViewManager(mContext, this);
+        } else if(!mLockscreenNotifications && mNotificationViewManager != null) {
+            mNotificationViewManager.unregisterListeners();
+            mNotificationViewManager = null;
+        }
+
     	mBlurEnabled = Settings.System.getInt(mContext.getContentResolver(),
     			Settings.System.LOCKSCREEN_BLUR_BEHIND, 0) == 1;
 
@@ -650,10 +669,19 @@ public class KeyguardViewManager {
         mKeyguardView.initializeSwitchingUserState(options != null &&
                 options.getBoolean(IS_SWITCHING_USER));
 
+	if (mLockscreenNotifications) {
+            mNotificationView = (NotificationHostView)mKeyguardView.findViewById(R.id.notification_host_view);
+            mNotificationViewManager.setHostView(mNotificationView);
+            mNotificationViewManager.onScreenTurnedOff();
+        }
+
         // HACK
         // The keyguard view will have set up window flags in onFinishInflate before we set
         // the view mediator callback. Make sure it knows the correct IME state.
         if (mViewMediatorCallback != null) {
+	    if (mLockscreenNotifications)
+                mNotificationView.setViewMediator(mViewMediatorCallback);
+
             KeyguardPasswordView kpv = (KeyguardPasswordView) mKeyguardView.findViewById(
                     R.id.keyguard_password_view);
 
@@ -751,6 +779,9 @@ public class KeyguardViewManager {
         if (mKeyguardView != null) {
             mKeyguardView.onScreenTurnedOff();
         }
+	if (mLockscreenNotifications) {
+            mNotificationViewManager.onScreenTurnedOff();
+        }
     }
 
     public synchronized void onScreenTurnedOn(final IKeyguardShowCallback callback) {
@@ -798,6 +829,9 @@ public class KeyguardViewManager {
                 Slog.w(TAG, "Exception calling onShown():", e);
             }
         }
+	if (mLockscreenNotifications) {
+            mNotificationViewManager.onScreenTurnedOn();
+        }
     }
 
     public synchronized void verifyUnlock() {
@@ -811,6 +845,10 @@ public class KeyguardViewManager {
      */
     public synchronized void hide() {
         if (DEBUG) Log.d(TAG, "hide()");
+
+	if (mLockscreenNotifications) {
+            mNotificationViewManager.onDismiss();
+        }
 
         if (mKeyguardHost != null) {
             mKeyguardHost.setVisibility(View.GONE);
