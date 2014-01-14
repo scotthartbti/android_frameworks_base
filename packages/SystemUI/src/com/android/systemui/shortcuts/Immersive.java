ff94e9daa5a4d7d8b6b406e8e4eed5b1820a15ac
Jubakuba <Jubakuba@gmail.com>
2014-01-14 03:20:34 -0700

    SystemUI: Extra Slim Shortcuts && Cleanup
    Some cleanup of slim shortcuts
    	& implement method to allow
    	user to toggle toggleable
    	settings to the preferred state.
    	Much easier to use with confidence
    	knowing you'll be in the correct state.
    
    Change-Id: I897ee4567426d0314952b9860a487ab445795dfc
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	packages/SystemUI/res/values/bs_strings.xml
    	packages/SystemUI/src/com/android/systemui/shortcuts/Immersive.java
    	packages/SystemUI/src/com/android/systemui/shortcuts/Reboot.java

--- packages/SystemUI/src/com/android/systemui/shortcuts/Immersive.java
+++ packages/SystemUI/src/com/android/systemui/shortcuts/Immersive.java
@@ -20,8 +20,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.android.internal.util.beanstalk.PolicyConstants;
-import com.android.internal.util.beanstalk.SlimActions;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 public class Immersive extends Activity  {
 
@@ -33,8 +33,19 @@ public class Immersive extends Activity  {
     @Override
     public void onResume() {
         super.onResume();
-        SlimActions.processAction(
-                this, PolicyConstants.ACTION_EXPANDED_DESKTOP, false);
+        int value = getIntent().getIntExtra("value", 2);
+
+        if (value == 2) {
+            value = Settings.System.getIntForUser(
+                    getContentResolver(),
+                    Settings.System.EXPANDED_DESKTOP_STATE,
+                    0, UserHandle.USER_CURRENT_OR_SELF) == 1 ? 0 : 1;
+        }
+
+        Settings.System.putIntForUser(
+                getContentResolver(),
+                Settings.System.EXPANDED_DESKTOP_STATE,
+                value, UserHandle.USER_CURRENT_OR_SELF);
         this.finish();
     }
 }
