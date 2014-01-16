cf69dee0e0104945d62eba8ba136e3c5fb497800
David van Tonder <david.vantonder@gmail.com>
2014-01-16 12:42:48 +0000

    Merge "Camera button support (1/2)" into cm-11.0
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/config.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Change-Id: I4bcecf5633b7656f894a80d312c354a5bc1decb0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3436,6 +3436,24 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Whether to wake the screen with the camera key half-press.
+         * @hide
+         */
+        public static final String CAMERA_WAKE_SCREEN = "camera_wake_screen";
+
+        /**
+         * Whether or not to send device back to sleep if Camera button is released ("Peek")
+         * @hide
+         */
+        public static final String CAMERA_SLEEP_ON_RELEASE = "camera_sleep_on_release";
+
+        /**
+         * Whether or not camera button music controls should be enabled to play/pause media tracks
+         * @hide
+         */
+        public static final String CAMERA_MUSIC_CONTROLS = "camera_music_controls";
+
+        /**
          * Whether or not to launch default music player when headset is connected
          * @hide
          */
@@ -3653,6 +3671,7 @@ public final class Settings {
           * 3 - Search
           * 4 - Voice search
           * 5 - In-app search
+	  * 6 - Launch Camera
           * @hide
           */
          public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
@@ -4421,6 +4440,12 @@ public final class Settings {
          * @hide
          */
         public static final String HOME_UNLOCK_SCREEN = "home_unlock_screen";
+
+        /**
+         * Whether to unlock the screen with the camera key.  The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String CAMERA_UNLOCK_SCREEN = "camera_unlock_screen";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
