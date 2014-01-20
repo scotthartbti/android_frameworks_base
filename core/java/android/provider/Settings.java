49d95ef6b67f9f027e82783a3e25b3557164cc91
scott <scott@ubuntu>
2014-01-20 01:00:17 -0500

    Remove duplicate in settings.java
    Change-Id: I91fe49fc822efc69760950d4a3ddd29518fce5d8
    Signed-off-by: scott <scott@ubuntu>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3057,13 +3057,6 @@ public final class Settings {
         public static final String QS_QUICK_PULLDOWN = "qs_quick_pulldown";
 
         /**
-         * Quick Settings Collapse Pane
-         *
-         * @hide
-         */
-        public static final String QS_COLLAPSE_PANEL = "qs_collapse_panel";
-
-        /**
          * Whether to hide the notification screen after clicking on a widget
          * button
          *
