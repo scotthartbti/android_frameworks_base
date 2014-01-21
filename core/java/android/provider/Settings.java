f50b176655f79ca2a57af11270d675223eff2ed1
Jon Haus <haus.xda@gmail.com>
2014-01-21 15:50:05 -0500

    [1/2] Base: Upgrade network traffic meter
    *Updated class name to be less ambiguous
    *Replaced the multi-check in settings with a proper listview
    *Added the ability to view up-stream, down-stream or both
    *Added the option to change how often it refreshes
    *Added a check before it updates the status bar to prevent it from
    refreshing everything needlessly
    *Added icons to show data direction
    *Remove unused style attributes
    
    Icon artwork is courtesy android design downloads
    Some features were inspired by previous work done by 0xD34D
    
    OmniGears: https://gerrit.omnirom.org/#/c/5130/
    
    Change-Id: I9f86292a6adbc5e30967df76fae510be248fc38b
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3081,13 +3081,8 @@ public final class Settings {
          * @hide
          */
         public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
-
-       /**
-         * Network traffic indicator, goes from least to greatest significant bitwise
-         * 0 = Display up-stream traffic if set
-         * 1 = Display down-stream traffic if set
-         * 2 = Show as Byte/s if set
-         * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
+	
+	/*
          * @hide
          */
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
@@ -3160,13 +3155,15 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
-        /** Network speed indicator
-         * 0 = disabled
-         * 1 = show bits per second
-         * 2 = show Bytes per second
+       /**
+         * Network traffic indicator, goes from least to greatest significant bitwise
+         * 0 = Display up-stream traffic if set
+         * 1 = Display down-stream traffic if set
+         * 2 = Show as Byte/s if set
+         * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
          * @hide
          */
-        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
