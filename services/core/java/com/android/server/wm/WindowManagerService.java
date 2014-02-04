d25c9ce1d814ac92cfaf11a8d59d60a48c94b39b
Dave Kessler AlmightyMegadeth00 <activethrasher00@gmail.com>
2014-02-04 15:53:28 -0700

    FWB - Add PowerMenu QS Tile (Reboot on longpress) [1/2]
    Porting from KK to LP
    
    PS2: whiten icons and add blur
    
    Change-Id: I6e44b91ce14b3c732c990624fba381b8b257f859
    Signed-off-by: STELIX <ssspinni@gmail.com>
    Signed-off-by: scott <scott@beanstalk.com>
    
    Conflicts:
    	packages/SystemUI/res/values/cm_strings.xml

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5605,6 +5605,12 @@ public class WindowManagerService extends IWindowManager.Stub
 
     // Called by window manager policy.  Not exposed externally.
     @Override
+    public void rebootTile() {
+        ShutdownThread.reboot(mContext, null, true);
+    }
+
+    // Called by window manager policy.  Not exposed externally.
+    @Override
     public void rebootSafeMode(boolean confirm) {
         ShutdownThread.rebootSafeMode(mContext, confirm);
     }
@@ -10876,7 +10882,7 @@ public class WindowManagerService extends IWindowManager.Stub
         return mPolicy.hasPermanentMenuKey();
     }
 
-    @Override 
+    @Override
     public boolean needsNavigationBar() {
         return mPolicy.needsNavigationBar();
     }
