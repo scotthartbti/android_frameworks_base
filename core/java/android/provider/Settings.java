00696220a0f953a8ff5a250106edc7b90de6fe83
cphelps76 <cphelps76@droidconcepts.com>
2014-02-07 16:15:10 +0100

    Frameworks: Port BatteryBar (1/2)
    {x} Adds Battery bar with following features:
    {x} location set
    {x} color
    {x} aligment
    {x} thickness
    {x} charging animation
    
    Change-Id: Ie9a168121dc569ec3af068a4bda6b46a4e896b2d
    
    Frameworks: update battery bar default color to match kitkat ui (1/2)
    
    Change-Id: I4fc0b51abb29ac0a9f0b86e4d0ad3519f697b0f6
    Signed-off-by: scott <scott@ubuntu.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout-sw600dp/navigation_bar.xml
    	packages/SystemUI/res/layout/navigation_bar.xml
    
    Conflicts:
    	packages/SystemUI/res/drawable/ic_recents_clear.xml
    	packages/SystemUI/res/layout/navigation_bar.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values-mcc232-mnc12/config.xml
    	packages/SystemUI/res/drawable/ic_notification_reminder_bg.xml
    	packages/SystemUI/res/layout/navigation_bar.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4317,6 +4317,32 @@ public final class Settings {
         public static final String SYSTEM_POWER_CRT_MODE = "system_power_crt_mode";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+        /**
          * Circle battery icon color
          * in statusbar
          * @hide
