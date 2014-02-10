f54acd357df756135cb5183bc943654359398a9e
scott <scott@ubuntu>
2014-02-10 00:31:59 -0500

    fixup torchconstant location
    Change-Id: Ia3f6fdc08fc9ab13422bc561184d9dd5f04790fb
    Signed-off-by: scott <scott@ubuntu>

--- packages/SystemUI/src/com/android/systemui/shortcuts/Torch.java
+++ packages/SystemUI/src/com/android/systemui/shortcuts/Torch.java
@@ -21,7 +21,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.UserHandle;
 
-import com.android.internal.util.beanstalk.TorchConstants;
+import com.android.internal.util.cm.TorchConstants;
 
 public class Torch extends Activity  {
 
