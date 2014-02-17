754077202fd9f88378073cf038078f51dabbb17a
Steve Kondik <shade@chemlab.org>
2014-02-17 13:49:16 -0800

    telephony: Cleanup after QCRIL merge
    Change-Id: I777818d9034593696bf96ad96048daa0011e9f80

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -227,8 +227,6 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
                 com.android.internal.R.string.lockscreen_carrier_default);
         mNetworkName = mNetworkNameDefault;
 
-        createWifiHandler();
-
         // broadcasts
         IntentFilter filter = new IntentFilter();
         filter.addAction("com.android.settings.LABEL_CHANGED");
@@ -296,6 +294,7 @@ public class NetworkController extends BroadcastReceiver implements DemoMode {
 
     public void addDataTypeIconView(ImageView v) {
         mDataTypeIconViews.add(v);
+    }
 
     protected void registerPhoneStateListener(Context context) {
         // telephony
