42c6154e175c632a85378177d1b07b674f98e71a
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-09 18:51:11 +0700

    [1/2] Base: ActiveDisplay bugfixes
    1. fix respect QuietHour changes
    2. fix compute background bound for background image
    3. fix http://jira.omnirom.org/browse/OMNI-724
    4. fix http://jira.omnirom.org/browse/OMNI-722
    5. fix http://jira.omnirom.org/browse/OMNI-709
    6. remove useless layout
    7. fix invalidation when sunlight mode disabled
    8. fix fetch wallpaper
    9. fix glowpad ring after configuration changes
    10. disable wallpaper blur until we find fix for problem in OMNI-715
    11. make sure when configuration changes, all view is rebuild, so there is double background window
        is showing
    12. remove Wallpaper
    
    Change-Id: I15fc9a6cb21e009f92952477ced34cfaf37b8f38
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/Android.mk

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2912,12 +2912,6 @@ public final class Settings {
 	 * @hide
 	 */
 	public static final String ACTIVE_DISPLAY_BYPASS = "active_display_bypass";
-	
-	/**
-         * Whether to showing wallpaper behind active display.
-         * @hide
-         */
-        public static final String ACTIVE_DISPLAY_WALLPAPER = "active_display_wallpaper";
 
         /**
          * Whether to not showing active display when there is annoying notifications.
