145e50194ebd6ff3bd1b4fe37a7e1418ce638cde
Steve Kondik <shade@chemlab.org>
2014-04-01 23:41:42 -0700

    bootanim: Add continuous splash support
     * We may or may not want to have a very smooth transition from the
       bootloader into Android by way of a continuous splash feature.
     * Add a compile-time flag which disables the initial screen clearing
       so that we don't drop a frame during the transition.
    
    Change-Id: Ic453c901f1030ffebd9bdbeec59109bb5c585629

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -514,6 +514,7 @@ bool BootAnimation::movie()
         }
     }
 
+#ifndef CONTINUOUS_SPLASH
     // clear screen
     glShadeModel(GL_FLAT);
     glDisable(GL_DITHER);
@@ -523,6 +524,7 @@ bool BootAnimation::movie()
     glClear(GL_COLOR_BUFFER_BIT);
 
     eglSwapBuffers(mDisplay, mSurface);
+#endif
 
     glBindTexture(GL_TEXTURE_2D, 0);
     glEnable(GL_TEXTURE_2D);
