78032af6afdb1464ae618f60a17055e1939dac27
Lars Greiss <kufikugel@googlemail.com>
2014-04-18 12:10:27 +0200

    Revert "Insufficient ProcessRecord cleanup when persistent process is killed"
    This reverts commit 72e5fb0277ebb6149b31acc354c88dab40ce977f.
    
    Change-Id: I7f9c3024b429a9667d8639f0a8804e21e77612f7

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -12563,7 +12563,6 @@ public final class ActivityManagerService extends ActivityManagerNative
         app.foregroundActivities = false;
         app.hasShownUi = false;
         app.hasAboveClient = false;
-        app.hasClientActivities = false;
 
         mServices.killServicesLocked(app, allowRestart);
 
