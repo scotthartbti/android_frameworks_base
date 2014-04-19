c4e5e15887d0622ff60199ceb14eab4a0ada6117
scott <scott@ubuntu>
2014-04-19 02:01:09 -0400

    fixup navbar commit
    Change-Id: Ieec2178fd091fbc5f522168c3525380acc737971
    Signed-off-by: scott <scott@ubuntu>

--- core/java/com/android/internal/util/beanstalk/SlimActions.java
+++ core/java/com/android/internal/util/beanstalk/SlimActions.java
@@ -108,16 +108,16 @@ public class SlimActions {
                 injectKeyDelayed(KeyEvent.KEYCODE_MENU, isLongpress, false);
                 return;
             } else if (action.equals(ButtonsConstants.ACTION_IME_NAVIGATION_LEFT)) {
-                triggerVirtualKeypress(KeyEvent.KEYCODE_DPAD_LEFT, isLongpress);
+                injectKeyDelayed(KeyEvent.KEYCODE_DPAD_LEFT, isLongpress, false);
                 return;
             } else if (action.equals(ButtonsConstants.ACTION_IME_NAVIGATION_RIGHT)) {
-                triggerVirtualKeypress(KeyEvent.KEYCODE_DPAD_RIGHT, isLongpress);
+                injectKeyDelayed(KeyEvent.KEYCODE_DPAD_RIGHT, isLongpress, false);
                 return;
             } else if (action.equals(ButtonsConstants.ACTION_IME_NAVIGATION_UP)) {
-                triggerVirtualKeypress(KeyEvent.KEYCODE_DPAD_UP, isLongpress);
+                injectKeyDelayed(KeyEvent.KEYCODE_DPAD_UP, isLongpress, false);
                 return;
             } else if (action.equals(ButtonsConstants.ACTION_IME_NAVIGATION_DOWN)) {
-                triggerVirtualKeypress(KeyEvent.KEYCODE_DPAD_DOWN, isLongpress);
+                injectKeyDelayed(KeyEvent.KEYCODE_DPAD_DOWN, isLongpress, false);
                 return;
             } else if (action.equals(ButtonsConstants.ACTION_POWER_MENU)) {
                 try {
