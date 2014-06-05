5249cb28690d77423f5f384b26cf2e322ec8263c
liquid0624 <rjbirnesser@gmail.com>
2014-06-05 19:21:15 -0400

    Fix setting confirmation helper derp
    AICPfy
    
    Change-Id: I1773ffa95bd32b822a58ae522f0cc038ea4425e5

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
@@ -404,7 +404,7 @@ public class Hover {
 
         if (currentNotification != null && !isKeyguardSecureShowing() && !isStatusBarExpanded()
                 && mHoverActive && !mShowing && isScreenOn() && !isSimPanelShowing()
-                && !mNotificationHelper.isPeekShowing() && !mNotificationHelper.isPeekAppShowing()) {
+                && !mNotificationHelper.isPeekShowing()) {
             if (isRingingOrConnected() && isDialpadShowing()) {
                 // incoming call notification has been already processed,
                 // and since we don't want to show other ones, clear and return.
@@ -441,7 +441,7 @@ public class Hover {
                 public void onAnimationStart(Animator animation) {
                     if (isStatusBarExpanded() | (isRingingOrConnected() && isDialpadShowing())
                             | isKeyguardSecureShowing() | mNotificationHelper.isPeekShowing()
-                            | !isScreenOn() | isSimPanelShowing() | mNotificationHelper.isPeekAppShowing()) {
+                            | !isScreenOn() | isSimPanelShowing()) {
                         clearHandlerCallbacks();
                         setAnimatingVisibility(false);
                         dismissHover(true, true);
@@ -452,7 +452,7 @@ public class Hover {
                 public void onAnimationEnd(Animator animation) {
                     if (isStatusBarExpanded() | (isRingingOrConnected() && isDialpadShowing())
                             | isKeyguardSecureShowing() | mNotificationHelper.isPeekShowing()
-                            | !isScreenOn() | isSimPanelShowing() | mNotificationHelper.isPeekAppShowing()) {
+                            | !isScreenOn() | isSimPanelShowing()) {
                         clearHandlerCallbacks();
                         setAnimatingVisibility(false);
                         dismissHover(false, true);
