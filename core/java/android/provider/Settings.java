78ea7d2f97da9653b0833c7c77479d08ce768851
Raj Yengisetty <rajesh@cyngn.com>
2014-06-12 17:52:22 -0700

    Add the appropriate @hide flag so we can build!
    Change-Id: Id45f682287741badb13e256f6029c52820a068bd

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7512,6 +7512,7 @@ public final class Settings {
 
         /**
          * Protected Components
+         * @hide
          */
         public static final String PROTECTED_COMPONENTS = "protected_components";
 
