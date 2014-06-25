ac824f9c818b81cb84842c1a5d23982a1f75aa0b
BigBrother1984 <stevewatersy@gmail.com>
2014-06-25 15:53:42 +0200

    HOVER: Don't process and/or show foreground app notifications
    - Don't show notification of foreground app
    - Animate the ticker in this case so user can see it
    
    Inspired from changes linked here https://gerrit.paranoidandroid.co/#/c/4453/10
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I9f45e1641a7478c7e460c32133db00bd67f63e75
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/Hover.java
@@ -398,7 +398,10 @@ public class Hover {
     }
 
     public void showCurrentNotification() {
+        clearForegroundAppNotifications();
+
         final HoverNotification currentNotification = getHoverNotification(INDEX_CURRENT);
+
         if (currentNotification != null && !isKeyguardSecureShowing() && !isStatusBarExpanded()
                 && mHoverActive && !mShowing && isScreenOn() && !isSimPanelShowing()
                 && !mNotificationHelper.isPeekShowing() && !mNotificationHelper.isPeekAppShowing()) {
@@ -469,6 +472,8 @@ public class Hover {
     }
 
     private void overrideShowingNotification() {
+        clearForegroundAppNotifications();
+
         final HoverNotification currentNotification = getCurrentNotification();
         final HoverNotification nextNotification = getHoverNotification(INDEX_NEXT);
 
@@ -634,10 +639,11 @@ public class Hover {
 
     // notifications processing
     public void setNotification(Entry entry, boolean update) {
-        // first, check if current notification's package is blacklisted or excluded in another way
+        // first, check if current notification's package
+        // is blacklisted and/or comes from foreground app
         boolean allowed = true; // default on
+        boolean foreground = false; // default off
 
-        //Exclude blacklisted
         try {
             final String packageName = entry.notification.getPackageName();
             allowed = mStatusBar.getNotificationManager().isPackageAllowedForHover(packageName);
@@ -672,7 +678,11 @@ public class Hover {
             allowed = false;
         }
 
-        if (!allowed) {
+        if (entry.notification.getPackageName().equals(
+                mNotificationHelper.getForegroundPackageName()))
+        foreground = true;
+
+        if (!allowed | foreground) {
             addStatusBarNotification(entry.notification);
             return;
         }
@@ -859,6 +869,15 @@ public class Hover {
 
     public void clearNotificationList() {
         reparentAllNotifications();
+    }
+
+    public void clearForegroundAppNotifications() {
+        for (int i = 0; i < mNotificationList.size(); i++) {
+            if (mNotificationList.get(i).getContent().getPackageName()
+                    .equals(mNotificationHelper.getForegroundPackageName())) {
+                mNotificationList.remove(i);
+            }
+        }
     }
 
     public void reparentAllNotifications() {
