54a28c31416408c94424f8fd3aba901686ac87d3
Danesh M <daneshm90@gmail.com>
2014-06-25 21:31:49 -0700

    ProximityWake : Add support for checking proximity when waking device [1/2]
    Change-Id: Ia5ff446d88435773acccc7467e66009000dc20cb
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/os/IPowerManager.aidl
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/power/PowerManagerService.java
    
    Conflicts:
    	core/java/android/os/IPowerManager.aidl
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/os/IPowerManager.aidl
    	services/java/com/android/server/power/PowerManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5740,6 +5740,11 @@ public final class Settings {
         public static final String RECENT_PANEL_BG_COLOR = "recent_panel_bg_color";
 
         /**
+         * @hide
+         */
+        public static final String PROXIMITY_ON_WAKE = "proximity_on_wake";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
