069b431bd5020434603466d0af88abf9b16814cd
scott <scott@ubuntu>
2014-08-31 22:47:40 -0400

    NFC Polling Mode (3/3)
    Change-Id: Ib411b5f9497821e19c5c3b22a2c258ce3dbbd308
    Signed-off-by: scott <scott@ubuntu>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3139,6 +3139,8 @@ public final class Settings {
          */
         public static final String POINTER_SPEED = "pointer_speed";
 
+	public static final String NFC_POLLING_MODE = "nfc_polling_mode";
+
        /**
 	* Use the Notification Power Widget? (Who wouldn't!)
 	*
