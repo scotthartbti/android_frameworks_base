ff8d524637517a1880268612f269ff091201996a
scott <scott@ubuntu>
2014-10-26 23:49:38 -0400

    fix from carrier logo commit
    Change-Id: Id4862e36f3c6b5889aab44067c08dfe0d154307e
    Signed-off-by: scott <scott@ubuntu>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -39,6 +39,7 @@ import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
