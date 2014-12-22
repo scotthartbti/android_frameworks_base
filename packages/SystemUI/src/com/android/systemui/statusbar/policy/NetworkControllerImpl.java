829332336ce775310585c856f398b0acc68bcd90
PrimeDirective <activethrasher00@gmail.com>
2014-12-22 21:31:14 +0100

    Wifi notify: Remove 1 line of code
    Change-Id: I13b5cc7fdbff4d4ba52a1caad7c741ba877cc35a

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -1203,7 +1203,6 @@ public class NetworkControllerImpl extends BroadcastReceiver
                         }
                         if (!restoredConnection) mConnectionsList.add(mWifiSsid);
                     }
-                    mConnectionAtBoot = false;
                 } else {
                     mWifiSsid = null;
                 }
