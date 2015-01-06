817105a98cbbef86ef24852f7159abcb0aa1a73f
Daxxmax <>
2015-01-06 08:47:44 +0100

    FWB: Statusbar clock font style (1/2)
    AICPfy
    Bring back to L
    
    Change-Id: Iafb811b9f819a100940fdeeaa2c657483c039950
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3564,6 +3564,12 @@ public final class Settings {
         public static final String APP_CIRCLE_BAR_SHOW_TRIGGER = "app_circle_bar_show_trigger";
 
         /**
+         * Settings for clock font style
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
