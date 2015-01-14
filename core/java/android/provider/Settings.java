8567638942f6b872b4497af67465aeba72cf6849
Brian Larsen <brian@chezbel.com>
2015-01-14 14:10:11 +0800

    FWB: Hide camera in lockscreen (1/2)
    Added a setting in "AICP Extras" / "Various Shit" to control whether or not
    to display the camera widget on the lockscreen.
    
    Credit to Davor Bertovic for the following abandoned patches
    
    PS2:
    Removed get and bupBoolean
    Made multiuser compatible
    PS3:
    Edit commit message
    
    Change-Id: I13e6d0f52afcea17fde71365fc2a4a65cebd0cec
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3687,6 +3687,14 @@ public final class Settings {
          */
         public static final String APP_CIRCLE_BAR_SHOW_TRIGGER = "app_circle_bar_show_trigger";
 
+       /**
+         * Allows to disable Camera widget in locskcreen
+         * 0 = use system default (show)
+         * 1 = hide camera
+         * @hide
+         */
+        public static final String CAMERA_WIDGET_HIDE = "camera_widget_hide";
+
         /**
          * Settings for clock font style
          * @hide
@@ -4614,6 +4622,7 @@ public final class Settings {
                 int userHandle) {
             return putStringForUser(cr, name, Float.toString(value), userHandle);
         }
+
 
         /**
          * @deprecated Use {@link android.provider.Settings.Global#DEVELOPMENT_SETTINGS_ENABLED}
