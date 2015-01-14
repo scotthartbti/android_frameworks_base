401dfd8c2e9f4c7cfa09bbdfc691c9e33ff738b4
LorDClockaN <davor@losinj.com>
2015-01-14 23:58:36 +0100

    Revert "Frameworks: Add reboot menu - prepare for Slims Global menu - misc changes"
    This reverts commit 43ca3a015442110a42ff7f16093a3ea953e603e7.
    
    Change-Id: Ida16676b54056f60f9a365cbfa4a1e9d599b824c
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/res/res/values/liquid_arrays.xml
    	core/res/res/values/slim_strings.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5575,12 +5575,6 @@ public class WindowManagerService extends IWindowManager.Stub
         }
     }
 
-    // Called by window manager policy. Not exposed externally.
-    @Override
-    public void reboot() {
-        ShutdownThread.reboot(mContext, null, true);
-    }
-
     public void setCurrentUser(final int newUserId, final int[] currentProfileIds) {
         synchronized (mWindowMap) {
             mCurrentUserId = newUserId;
