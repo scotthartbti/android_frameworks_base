3c0a6753350b9abcfeba3122d33c9aa2352f00bc
Andrew Boren <andrew.boren@gmail.com>
2015-01-25 09:19:16 +0100

    Powermenu screenshot delay [1/2]
     * Only works from power menu
     * Defaults to 1 second (1000ms)
    
    Change-Id: Ic7477d4ed962b97d5bbfd980cfdb7e3516262463

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3988,6 +3988,12 @@ public final class Settings {
                 "pie_buttons_config_second_layer";
 
         /**
+         * Change the screenshot delay
+         * @hide
+         */
+        public static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
