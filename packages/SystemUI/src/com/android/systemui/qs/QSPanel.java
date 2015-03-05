676b6599e058114d04df45e806a85cfe9bd1f047
Roman Birg <roman@cyngn.com>
2015-03-04 18:33:24 -0800

    SystemUI: improve qs tile scroll handling
      - place detail view within bounds of expanded panel when it
       goes beyond the bottom of the screen
    
      - collapse qs panel when scrolling detail view (and letting go),
        disallowing it to get stuck at weird states that cut off the detail view
    
    Change-Id: I775c0bb0022893a6a79fab93fb6cd92ba67b7213
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: scott <scott@beanstalk.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/qs/QSPanel.java

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.graphics.Point;
 import android.graphics.PorterDuff.Mode;
 import android.os.Handler;
 import android.os.Message;
@@ -77,6 +78,7 @@ public class QSPanel extends ViewGroup {
     private int mPanelPaddingBottom;
     private int mDualTileUnderlap;
     private int mBrightnessPaddingTop;
+    private int mTranslationTop;
     private boolean mExpanded;
     private boolean mListening;
 
@@ -97,6 +99,10 @@ public class QSPanel extends ViewGroup {
     private boolean mUseMainTiles = false;
 
     protected Vibrator mVibrator;
+
+    private DetailCallback mDetailCallback;
+    private int mContainerTop;
+
     private SettingsObserver mSettingsObserver;
 
     public QSPanel(Context context) {
@@ -202,6 +208,14 @@ public class QSPanel extends ViewGroup {
 
     public void setCallback(Callback callback) {
         mCallback = callback;
+    }
+
+    public void setDetailCallback(DetailCallback callback) {
+        mDetailCallback = callback;
+    }
+
+    public void setTopOfContainer(int t) {
+        mContainerTop = t;
     }
 
     public void setHost(QSTileHost host) {
@@ -430,7 +444,7 @@ public class QSPanel extends ViewGroup {
         }
         int x = r.tileView.getLeft() + r.tileView.getWidth() / 2;
         int y = r.tileView.getTop() + r.tileView.getHeight() / 2;
-        handleShowDetailImpl(r, show, x, y);
+        handleShowDetailImpl(r, show, x, y - mTranslationTop);
     }
 
     private void handleShowDetailImpl(Record r, boolean show, int x, int y) {
@@ -515,9 +529,22 @@ public class QSPanel extends ViewGroup {
         if (mFooter.hasFooter()) {
             h += mFooter.getView().getMeasuredHeight();
         }
+
         mDetail.measure(exactly(width), MeasureSpec.UNSPECIFIED);
-        if (mDetail.getMeasuredHeight() < h) {
-            mDetail.measure(exactly(width), exactly(h));
+        if (isShowingDetail()) {
+            Point size = new Point();
+            getDisplay().getSize(size);
+            final int panelBottom = (mContainerTop - mTranslationTop + h);
+            final int detailMinHeight = size.y - mContainerTop - mPanelPaddingBottom;
+
+            if (size.y > panelBottom) {
+                int delta = size.y - panelBottom;
+                // panel is smaller than screen size
+                mDetail.measure(exactly(width), exactly(detailMinHeight - delta));
+            } else {
+                // panel is hanging below the screen
+                mDetail.measure(exactly(width), exactly(detailMinHeight));
+            }
         }
         setMeasuredDimension(width, Math.max(h, mDetail.getMeasuredHeight()));
     }
@@ -551,7 +578,7 @@ public class QSPanel extends ViewGroup {
             record.tileView.layout(left, top, right, top + record.tileView.getMeasuredHeight());
         }
         final int dh = Math.max(mDetail.getMeasuredHeight(), getMeasuredHeight());
-        mDetail.layout(0, 0, mDetail.getMeasuredWidth(), dh);
+        mDetail.layout(0, mTranslationTop, mDetail.getMeasuredWidth(), dh + mTranslationTop);
         if (mFooter.hasFooter()) {
             View footer = mFooter.getView();
             footer.layout(0, getMeasuredHeight() - footer.getMeasuredHeight(),
@@ -578,6 +605,10 @@ public class QSPanel extends ViewGroup {
     private void fireShowingDetail(QSTile.DetailAdapter detail) {
         if (mCallback != null) {
             mCallback.onShowingDetail(detail);
+        }
+        if (mDetailCallback != null && detail != null) {
+            // don't notify when hiding, wait for animation to finish
+            mDetailCallback.onDetailChanged(true);
         }
     }
 
@@ -623,6 +654,12 @@ public class QSPanel extends ViewGroup {
         refreshAllTiles();
     }
 
+    public void setDetailOffset(int translationY) {
+        if (!isShowingDetail()) {
+            mTranslationTop = translationY;
+        }
+    }
+
     private class H extends Handler {
         private static final int SHOW_DETAIL = 1;
         private static final int SET_TILE_VISIBILITY = 2;
@@ -653,6 +690,9 @@ public class QSPanel extends ViewGroup {
         public void onAnimationEnd(Animator animation) {
             mDetailContent.removeAllViews();
             setDetailRecord(null);
+            if (mDetailCallback != null) {
+                mDetailCallback.onDetailChanged(false);
+            }
         };
     };
 
@@ -668,6 +708,10 @@ public class QSPanel extends ViewGroup {
             setGridContentVisibility(true);
         }
     };
+
+    public interface DetailCallback {
+        void onDetailChanged(boolean showing);
+    }
 
     public interface Callback {
         void onShowingDetail(QSTile.DetailAdapter detail);
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -225,6 +225,16 @@ public class NotificationPanelView extends PanelView implements
         mKeyguardStatusView = (KeyguardStatusView) findViewById(R.id.keyguard_status_view);
         mQsContainer = findViewById(R.id.quick_settings_container);
         mQsPanel = (QSPanel) findViewById(R.id.quick_settings_panel);
+        mQsPanel.setDetailCallback(new QSPanel.DetailCallback() {
+            @Override
+            public void onDetailChanged(boolean showing) {
+                mQsPanel.setTopOfContainer(mQsContainer.getTop());
+                mQsPanel.setDetailOffset(mScrollView.getScrollY());
+                if (!showing) {
+                    mHandler.removeCallbacks(mCloseQsRunnable);
+                }
+            }
+        });
         mClockView = (TextView) findViewById(R.id.clock_view);
         mScrollView = (ObservableScrollView) findViewById(R.id.scroll_view);
         mScrollView.setListener(this);
@@ -606,6 +616,9 @@ public class NotificationPanelView extends PanelView implements
     }
 
     private boolean flingExpandsQs(float vel) {
+        if (isQsDetailShowing() && mStatusBarState == StatusBarState.SHADE) {
+            return false;
+        }
         if (isBelowFalsingThreshold()) {
             return false;
         }
@@ -734,6 +747,9 @@ public class NotificationPanelView extends PanelView implements
         // If we are already running a QS expansion, make sure that we keep the panel open.
         if (mQsExpansionAnimator != null) {
             expands = true;
+        }
+        if (isQsDetailShowing()) {
+            expands = false;
         }
         return expands;
     }
@@ -971,7 +987,7 @@ public class NotificationPanelView extends PanelView implements
             return true;
         }
     };
-    
+
     private void animateHeaderSlidingIn() {
         mHeaderAnimatingIn = true;
         getViewTreeObserver().addOnPreDrawListener(mStartHeaderSlidingIn);
@@ -1596,9 +1612,30 @@ public class NotificationPanelView extends PanelView implements
     public void onReset(ExpandableView view) {
     }
 
+    private Runnable mCloseQsRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (isQsDetailShowing()
+                    && !mTracking
+                    && (!mQsTracking || mStatusBarState == StatusBarState.KEYGUARD)
+                    && !mIntercepting
+                    && mQsExpansionAnimator == null) {
+                if (mStatusBarState == StatusBarState.SHADE) {
+                    closeQsDetail();
+                }
+                animateCloseQs();
+            }
+        }
+    };
+
     @Override
     public void onScrollChanged() {
+        mQsPanel.setDetailOffset(mScrollView.getScrollY());
         if (mQsExpanded) {
+            if (isQsDetailShowing()) {
+                mHandler.removeCallbacks(mCloseQsRunnable);
+                mHandler.postDelayed(mCloseQsRunnable, 200);
+            }
             requestScrollerTopPaddingUpdate(false /* animate */);
             requestPanelHeightUpdate();
         }
