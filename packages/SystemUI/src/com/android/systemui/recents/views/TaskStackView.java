6668abd04f1086a49f8632b9f7ceef644fc68e42
scott <scott@beanstalk.com>
2015-03-09 23:03:31 -0400

    Revert "systemui: animate clear recents exit"
    This reverts commit 3679145d76fc116ad8d55a607d7c24b91a8a5d4c.

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -490,7 +490,7 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
 
         Task t = mStack.getTasks().get(mFocusedTaskIndex);
         TaskView tv = getChildViewForTask(t);
-        tv.dismissTask(0L);
+        tv.dismissTask();
     }
 
     private boolean dismissAll() {
@@ -511,15 +511,12 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                 }
 
                 // Remove visible TaskViews
-                long dismissDelay = 0;
                 int childCount = getChildCount();
-                int delay = mConfig.taskViewRemoveAnimDuration / childCount;
                 if (!dismissAll() && childCount > 1) childCount--;
                 for (int i = 0; i < childCount; i++) {
                     TaskView tv = (TaskView) getChildAt(i);
                     tasks.remove(tv.getTask());
-                    tv.dismissTask(dismissDelay);
-                    dismissDelay += delay;
+                    tv.dismissTask();
                 }
 
                 int size = tasks.size();
@@ -1218,7 +1215,7 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                         public void run() {
                             mStack.removeTask(t);
                         }
-                    }, 0L);
+                    });
                 } else {
                     // Otherwise, remove the task from the stack immediately
                     mStack.removeTask(t);
