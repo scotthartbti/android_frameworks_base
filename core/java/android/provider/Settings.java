0c295dc6f0be70a0be9166bd06ed3692830024fa
mike <mikew8978@gmail.com>
2015-10-24 03:43:32 -0400

    FWB: Navigation bar button color (1/2)
    original commit https://github.com/AOSIP/platform_frameworks_base/commit/14ddb7d2e9f503b965446d2c561102f0410f7bc4
    
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
    
    Change-Id: I2e13ddd683dec600250e4b8208a16a86cecbda8f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4001,6 +4001,12 @@ public final class Settings {
         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
 
         /**
+         * Navigation bar button color
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_TINT = "navigation_bar_tint";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
