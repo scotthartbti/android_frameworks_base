afb93a1d8b4a07369fbd4bf00d6b5bc9341de439
Lars Greiss <kufikugel@googlemail.com>
2015-10-27 01:50:17 -0400

    Base: Navigation bar options (1/2) -Toggle on/off -Height and width
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	services/core/java/com/android/server/policy/PhoneWindowManager.java
    
    Change-Id: Ie7511c918b833d503119f1ab0aa29f52a4f8eca3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3555,6 +3555,30 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
 
+	/**
+         * Navigation bar height when it is on protrait
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
+         * Navigation bar height when it is on landscape
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT_LANDSCAPE = "navigation_bar_height_landscape";
+
+        /**
+         * Navigation bar height when it is on landscape at the right
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
+
+        /**
+         * Wether navigation bar is enabled or not
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
