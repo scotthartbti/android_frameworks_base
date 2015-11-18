5cd05e79627596f372fd3e15e06367b4e904ffaf
Jmz <jmztaylor@gmail.com>
2015-11-18 16:25:17 +0100

    Force Expanded Notifications [1/2]
    This will force expanded notifications for all apps that support expanded notifications
    
    Change-Id: I2259ae02bbdeb0867b83a52277b5f7ff2de6485d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3495,6 +3495,12 @@ public final class Settings {
         public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
 
         /**
+         * Force expanded notifications on all apps that support it.
+         * @hide
+         */
+        public static final String FORCE_EXPANDED_NOTIFICATIONS = "force_expanded_notifications";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
