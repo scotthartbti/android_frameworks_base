a5c89fe0bddab8cf5fbbe622db9a5387c319478b
Patrick Lower <devvortex@gmail.com>
2015-12-03 12:43:18 -0500

    Revert "Proximity: Fix prevent accidental wakeup"
    This reverts commit 261ab20196bc7d681d9c2873bee386bc4adfb702.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -3392,7 +3392,11 @@ public final class PowerManagerService extends SystemService
                     }
                 }
             };
-            runWithProximityCheck(r);
+            if (checkProximity) {
+                runWithProximityCheck(r);
+            } else {
+                r.run();
+            }
         }
 
         private void runWithProximityCheck(Runnable r) {
