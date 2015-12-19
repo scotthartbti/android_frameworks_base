cbff2b0d6139828469b1948152e1eaccdcc47e5d
scott <scott@ubuntu>
2015-12-19 08:59:33 -0500

    TaskManager: Dont worry about recreating views since were restarting systemUI on toggle.  Temporary until more viable solution is found.
    Change-Id: I0666b39cba6be375202de3b148a6c7d4dac6246b
    Signed-off-by: scott <scott@ubuntu>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -509,11 +509,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                             mContext.getContentResolver(),
                             Settings.System.ENABLE_TASK_MANAGER,
                             0, UserHandle.USER_CURRENT) == 1;
-                            recreateStatusBar();
-                            updateRowStates();
-                            updateSpeedbump();
-                            updateClearAll();
-                            updateEmptyShadeView();
             }
 
             update();
