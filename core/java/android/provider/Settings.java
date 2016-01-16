5df159bf0f4d55690faef85fc489f8459a2989cf
Alex Cruz <mazdarider23@gmail.com>
2016-01-16 16:28:03 +0000

    Immersive Recents [1/2]
    This adds on to previous mod called FULL SCREEN RECENTS but now adds options
    to show recents with statusbar only or navbar only as well as fullscreen.
    
    - Renamed the mod to immersive recents to avoid confusion moving forward
    
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only
    
    Change-Id: Ifff1882ca9045773d257f009674a88f125b22763
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3584,10 +3584,14 @@ public final class Settings {
         public static final String RECENTS_SHOW_SEARCH_BAR = "recents_show_search_bar";
 
         /**
-         * Whether to display recents in full screen
-         * @hide
+         * Immersive recents options
+         *
+         * 0 = Default AOSP look
+         * 1 = Statusbar only
+         * 2 = Navbar only
+         * 3 = Fullscreen
          */
-        public static final String RECENTS_FULL_SCREEN = "recents_full_screen";
+        public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
         /**
          * Whether to display clock while recents are in full screen
