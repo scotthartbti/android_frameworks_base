3a54ecd76f8ba6b7bd2c9927c2f90d36ba0e4178
Gabriele M <moto.falcon.git@gmail.com>
2016-03-16 23:30:46 +0100

    SystemUI: Hide lockscreen visualizer when screen is turned off
    If the ACTION_KEYGUARD_WALLPAPER_CHANGED intent is received before
    the ACTION_SCREEN_OFF intent, the visualizer gets enabled as soon as
    the screen is turned off and never disabled.
    
    Change-Id: I017d043c60d1ba5ccc99f4dbe1d35e80b24334ee

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3797,6 +3797,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
             else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
                 mScreenOn = false;
+                mVisualizerView.setVisible(false);
                 notifyNavigationBarScreenOn(false);
                 notifyHeadsUpScreenOff();
                 finishBarAnimations();
