b30a3f535b1c94a07217cc8b0a09a8bb3c8a1425
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to quick settings tiles [1/2]
    Change-Id: I120c081b444e184e0ef7ae0476b42a35fbc9a96d
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: scott <scott.hart.bti@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4090,6 +4090,21 @@ public final class Settings {
          */
         public static final String TOAST_ANIMATION = "toast_animation";
 
+	/**
+         * Change quick settings tiles animation style
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_STYLE = "anim_tile_style";
+
+
+        /**
+         * Change quick settings tiles animation duration
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_DURATION = "anim_tile_duration";
+
         /**
          * Alternative recent apps integration using OmniSwitch
          * @hide
