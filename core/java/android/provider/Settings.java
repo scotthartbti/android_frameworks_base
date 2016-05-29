b1e4d069401aa2fc5616a3fee22f19a0d97da015
Daniel Huber <daniel.huber.business@gmail.com>
2016-05-29 21:49:37 +0200

    [1/2] base: Screenshot Crop & Share
    Change-Id: Iedec3d89362612a60a57b07884cae9de3466d1a9
    
    Screenshot Crop and share - Adjust for android 7
    
    Change-Id: I322aac65fa1d14da84328784c3a70dc81f0e2791
    
    base: Screenshot Crop & Share: add draw stroke width support
    
    Change-Id: I49944d57d4c315bdb58473165c996f8fd60c9682
    
    SystemUI: add edit action for screenshot notification
    
    to start the crop&share editor
    thanks to @LorDClockaN for the idea
    
    Change-Id: I15dd2f1a997fd84a5914a630ac3bfe6582fe9197
    
    SystemUI: fix wrong intent extra for screenshot edit
    
    missed one location
    
    Change-Id: I680bd1eb72b3ddb2d442d869fff7b1c37c977b02
    
    Screenshot crop and share - more fixes for N
    
    Change-Id: Ib83c61824c993adc6c7e4c99bdba11942bf82939
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3959,6 +3959,18 @@ public final class Settings {
          */
         public static final String SCREENSHOT_DELAY = "screenshot_delay";
 
+	/**
+         * Define if screenshot crop & share is active
+         * @hide
+         */
+        public static final String SCREENSHOT_CROP_AND_SHARE = "screenshot_crop_and_share";
+
+        /**
+         * Define the crop behavior of screenshot crop & share
+         * @hide
+         */
+        public static final String SCREENSHOT_CROP_BEHAVIOR = "screenshot_crop_behavior";
+
         /**
          * Toast animations
          *
