cf3a52ac690040ad9a4976fe0fec2232c7029203
Nico60 <ndhouailly@gmail.com>
2016-06-02 17:42:02 +0200

    Add interpolators to qs tiles animation [1/2]
    Change-Id: I9a34baeccc25a69abf5b9a1cddcd92c058a735d3
    Signed-off-by: scott <scott.hart.bti@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4105,6 +4105,13 @@ public final class Settings {
          */
         public static final String ANIM_TILE_DURATION = "anim_tile_duration";
 
+	/**
+         * Change quick settings tiles interpolator
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
+
         /**
          * Alternative recent apps integration using OmniSwitch
          * @hide
