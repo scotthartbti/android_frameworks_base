0e6a15b90e96926f1ff1bbd8ea2412a5944abf90
scott <scott.hart.bti@gmail.com>
2016-09-10 01:32:49 -0400

    Revert "Don't use timestamps with all zero"
    This reverts commit c324f4d429cc5b996844b23a950f9f28dd91aed6.

--- media/java/android/media/ExifInterface.java
+++ media/java/android/media/ExifInterface.java
@@ -17,8 +17,6 @@
 package android.media;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -129,9 +127,6 @@ public class ExifInterface {
     // they cannot keep state in the native code across function calls). We
     // use sLock to serialize the accesses.
     private static final Object sLock = new Object();
-
-    // Pattern to check non zero timestamp
-    private static final Pattern sNonZeroTimePattern = Pattern.compile(".*[1-9].*");
 
     /**
      * Reads Exif tags from the specified JPEG file.
@@ -372,8 +367,7 @@ public class ExifInterface {
      */
     public long getDateTime() {
         String dateTimeString = mAttributes.get(TAG_DATETIME);
-        if (dateTimeString == null
-                || !sNonZeroTimePattern.matcher(dateTimeString).matches()) return -1;
+        if (dateTimeString == null) return -1;
 
         ParsePosition pos = new ParsePosition(0);
         try {
@@ -408,9 +402,7 @@ public class ExifInterface {
     public long getGpsDateTime() {
         String date = mAttributes.get(TAG_GPS_DATESTAMP);
         String time = mAttributes.get(TAG_GPS_TIMESTAMP);
-        if (date == null || time == null
-                || (!sNonZeroTimePattern.matcher(date).matches()
-                && !sNonZeroTimePattern.matcher(time).matches())) return -1;
+        if (date == null || time == null) return -1;
 
         String dateTimeString = date + ' ' + time;
 
