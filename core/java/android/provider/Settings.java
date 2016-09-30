95fe8038184e6fc482c26dda0edeff8fdb88edf0
Alexander Martinz <eviscerationls@gmail.com>
2016-09-30 20:22:21 +0200

    Add Haptic Feedback to QS tiles [1/2]
    Change-Id: If358fb624032cd24d98fe2ea0c877f4875c5c151
    Signed-off-by: Scott <scott@ubuntu.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3780,6 +3780,13 @@ public final class Settings {
          */
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
+	/**
+         * Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
         /**
          * Network traffic indicator, goes from least to greatest significant bitwise
          * 0 = Display up-stream traffic if set
