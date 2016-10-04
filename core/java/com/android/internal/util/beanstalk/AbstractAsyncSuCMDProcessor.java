AICP/frameworks_base

Add Restart SystemUI to reboot menu
Add utils from Settings app needed for this to work.
Messy, but it works.

Change-Id: Ie00ffd52fec29e47ab8613d6feaf065542b40bd3

core/java/com/android/internal/util/aicp/AbstractAsyncSuCMDProcessor.java
@@ -1,0 +1,102 @@
+package com.android.internal.util.aicp;
+
+import android.os.AsyncTask;
+
+/**
+ * An abstract implentation of AsyncTask
+ *
+ * since our needs are simple send a command, perform a task when we finish
+ * this implentation requires you send the command as String...
+ * in the .execute(String) so you can send String[] of commands if needed
+ *
+ * This class is not for you if...
+ *     1) You do not need to perform any action after command execution
+ *        you want a Thread not this.
+ *     2) You need to perform more complex tasks in doInBackground
+ *        than simple script/command sequence of commands
+ *        you want your own AsyncTask not this.
+ *
+ * This class is for you if...
+ *     1) You need to run a command/script/sequence of commands without
+ *        blocking the UI thread and you must perform actions after the
+ *        task completes.
+ *     2) see #1.
+ */
+public abstract class AbstractAsyncSuCMDProcessor extends AsyncTask<String, Void, String> {
+    // if /system needs to be mounted before command
+    private boolean mMountSystem;
+    // return if we recieve a null command or empty command
+    public final String FAILURE = "failed_no_command";
+
+    /**
+     * Constructor that allows mounting/dismounting
+     * of /system partition while in background thread
+     */
+    public AbstractAsyncSuCMDProcessor(boolean mountSystem) {
+         this.mMountSystem = mountSystem;
+    }
+
+    /**
+     * Constructor that assumes /system should not be mounted
+     */
+    public AbstractAsyncSuCMDProcessor() {
+         this.mMountSystem = false;
+    }
+
+    /**
+     * DO NOT override this method you should simply send your commands off
+     * as params and expect to handle results in {@link #onPostExecute}
+     *
+     * if you find a need to @Override this method then you should
+     * consider using a new AsyncTask implentation instead
+     *
+     * @param params The parameters of the task.
+     *
+     * @return A result, defined by the subclass of this task.
+     */
+    @Override
+    protected String doInBackground(String... params) {
+        // don't bother if we don't get a command
+        if (params[0] == null || params[0].trim().equals(""))
+            return FAILURE;
+
+        String stdout = null;
+
+        // conditionally enforce mounting
+        if (mMountSystem) {
+            Helpers.getMount("rw");
+        }
+        try {
+            // process all commands ***DO NOT SEND null OR ""; you have been warned***
+            for (int i = 0; params.length > i; i++) {
+                // always watch for null and empty strings, lazy devs :/
+                if (params[i] != null && !params[i].trim().equals("")) {
+                    stdout = CMDProcessor.runSuCommand(params[i]).getStdout();
+                } else {
+                    // bail because of careless devs
+                    return FAILURE;
+                }
+            }
+        // always unmount
+        } finally {
+            if (mMountSystem)
+                Helpers.getMount("ro");
+        }
+        // return the stdout from the command
+        return stdout;
+    }
+
+    /**
+     * <p>Runs on the UI thread after {@link #doInBackground}. The
+     * specified result is the value returned by {@link #doInBackground}.</p>
+     *
+     * <p>This method won't be invoked if the task was cancelled.</p>
+     *
+     * You MUST @Override this method if you don't need the result
+     * then you should consider using a new Thread implentation instead
+     *
+     * @param result The result of the operation computed by {@link #doInBackground}.
+     */
+    @Override
+    protected abstract void onPostExecute(String result);
+}

core/java/com/android/internal/util/aicp/CMDProcessor.java
@@ -1,0 +1,69 @@
+package com.android.internal.util.aicp;
+
+import android.util.Log;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+// convenience import for quick referencing of this method
+
+public final class CMDProcessor {
+    private static final String TAG = "CMDProcessor";
+
+    private CMDProcessor() {
+        // Cannot instantiate this class
+        throw new AssertionError();
+    }
+
+    /* Run a system command with full redirection */
+    public static ChildProcess startSysCmd(String[] cmdarray, String childStdin) {
+        return new ChildProcess(cmdarray, childStdin);
+    }
+
+    public static CommandResult runSysCmd(String[] cmdarray, String childStdin) {
+        ChildProcess proc = startSysCmd(cmdarray, childStdin);
+        proc.waitFinished();
+        return proc.getResult();
+    }
+
+    public static ChildProcess startShellCommand(String cmd) {
+        String[] cmdarray = new String[3];
+        cmdarray[0] = "sh";
+        cmdarray[1] = "-c";
+        cmdarray[2] = cmd;
+        return startSysCmd(cmdarray, null);
+    }
+
+    public static CommandResult runShellCommand(String cmd) {
+        ChildProcess proc = startShellCommand(cmd);
+        proc.waitFinished();
+        return proc.getResult();
+    }
+
+    public static ChildProcess startSuCommand(String cmd) {
+        String[] cmdarray = new String[3];
+        cmdarray[0] = "su";
+        cmdarray[1] = "-c";
+        cmdarray[2] = cmd;
+        return startSysCmd(cmdarray, null);
+    }
+
+    public static CommandResult runSuCommand(String cmd) {
+        ChildProcess proc = startSuCommand(cmd);
+        proc.waitFinished();
+        return proc.getResult();
+    }
+
+    public static boolean canSU() {
+        CommandResult r = runShellCommand("id");
+        StringBuilder out = new StringBuilder(0);
+        out.append(r.getStdout());
+        out.append(" ; ");
+        out.append(r.getStderr());
+        Log.d(TAG, "canSU() su[" + r.getExitValue() + "]: " + out);
+        return r.success();
+    }
+}

core/java/com/android/internal/util/aicp/ChildProcess.java
@@ -1,0 +1,149 @@
+package com.android.internal.util.aicp;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+import static java.lang.System.nanoTime;
+
+public class ChildProcess {
+    private String TAG = getClass().getSimpleName();
+
+    private static final int PIPE_SIZE = 1024;
+
+    private class ChildReader extends Thread {
+        InputStream mStream;
+        StringBuffer mBuffer;
+        ChildReader(InputStream is, StringBuffer buf) {
+            mStream = is;
+            mBuffer = buf;
+        }
+        public void run() {
+            byte[] buf = new byte[PIPE_SIZE];
+            try {
+                int len;
+                while ((len = mStream.read(buf)) != -1) {
+                    String s = new String(buf, 0, len);
+                    mBuffer.append(s);
+                }
+            }
+            catch (IOException e) {
+                // Ignore
+            }
+            try {
+                mStream.close();
+            }
+            catch (IOException e) {
+                // Ignore
+            }
+        }
+    }
+    private class ChildWriter extends Thread {
+        OutputStream mStream;
+        String mBuffer;
+        ChildWriter(OutputStream os, String buf) {
+            mStream = os;
+            mBuffer = buf;
+        }
+        public void run() {
+            int off = 0;
+            byte[] buf = mBuffer.getBytes();
+            try {
+                while (off < buf.length) {
+                    int len = Math.min(PIPE_SIZE, buf.length - off);
+                    mStream.write(buf, off, len);
+                    off += len;
+                }
+            }
+            catch (IOException e) {
+                // Ignore
+            }
+            try {
+                mStream.close();
+            }
+            catch (IOException e) {
+                // Ignore
+            }
+        }
+    }
+
+    private long mStartTime;
+    private Process mChildProc;
+    private ChildWriter mChildStdinWriter;
+    private ChildReader mChildStdoutReader;
+    private ChildReader mChildStderrReader;
+    private StringBuffer mChildStdout;
+    private StringBuffer mChildStderr;
+    private int mExitValue;
+    private long mEndTime;
+
+    public ChildProcess(String[] cmdarray, String childStdin) {
+        mStartTime = nanoTime();
+        try {
+            mChildProc = Runtime.getRuntime().exec(cmdarray);
+            if (childStdin != null) {
+                mChildStdinWriter = new ChildWriter(mChildProc.getOutputStream(), childStdin);
+                mChildStdinWriter.start();
+            }
+            mChildStdout = new StringBuffer();
+            mChildStdoutReader = new ChildReader(mChildProc.getInputStream(), mChildStdout);
+            mChildStdoutReader.start();
+            mChildStderr = new StringBuffer();
+            mChildStderrReader = new ChildReader(mChildProc.getErrorStream(), mChildStderr);
+            mChildStderrReader.start();
+        }
+        catch (IOException e) {
+            // XXX: log
+        }
+    }
+
+    public boolean isFinished() {
+        boolean finished = true;
+        if (mChildProc != null) {
+            try {
+                mChildProc.exitValue();
+            }
+            catch (IllegalStateException e) {
+                finished = false;
+            }
+        }
+        return finished;
+    }
+
+    public int waitFinished() {
+        while (mChildProc != null) {
+            try {
+                mExitValue = mChildProc.waitFor();
+                mEndTime = nanoTime();
+                mChildProc = null;
+                mChildStderrReader.join();
+                mChildStderrReader = null;
+                mChildStdoutReader.join();
+                mChildStdoutReader = null;
+                if (mChildStdinWriter != null) {
+                    mChildStdinWriter.join();
+                    mChildStdinWriter = null;
+                }
+            }
+            catch (InterruptedException e) {
+                // Ignore
+            }
+        }
+        return mExitValue;
+    }
+
+    public CommandResult getResult() {
+        if (!isFinished()) {
+            throw new IllegalThreadStateException("Child process running");
+        }
+        return new CommandResult(
+                mStartTime,
+                mExitValue,
+                mChildStdout.toString(),
+                mChildStderr.toString(),
+                mEndTime);
+    }
+}

services/core/java/com/android/server/power/ShutdownThread.java
@@ -77,6 +77,8 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
+import com.android.internal.util.aicp.Helpers;
+
 public final class ShutdownThread extends Thread {
     // constants
     private static final String TAG = "ShutdownThread";
@@ -94,6 +96,7 @@ public final class ShutdownThread extends Thread {
     private static final int MOUNT_SERVICE_STOP_PERCENT = 20;
 
     private static final String SOFT_REBOOT = "soft_reboot";
+    private static final String SYSTEMUI_REBOOT = "systemui_reboot";
 
     // length of vibration before shutting down
     private static final int SHUTDOWN_VIBRATE_MS = 500;
@@ -255,7 +258,11 @@ public final class ShutdownThread extends Thread {
                                 if (selected != ListView.INVALID_POSITION) {
                                     String actions[] = context.getResources().getStringArray(
                                             com.android.internal.R.array.shutdown_reboot_actions);
-                                    if (selected >= 0 && selected < actions.length) {
+                                    if (actions[selected].equals(SYSTEMUI_REBOOT)) {
+                                        mReason = actions[selected];
+                                        doSystemUIReboot();
+                                        return;
+                                    } else if (selected >= 0 && selected < actions.length) {
                                         mReason = actions[selected];
                                         if (actions[selected].equals(SOFT_REBOOT)) {
                                             doSoftReboot();
@@ -292,6 +299,10 @@ public final class ShutdownThread extends Thread {
         } catch (RemoteException e) {
             Log.e(TAG, "failure trying to perform soft reboot", e);
         }
+    }
+
+    private static void doSystemUIReboot() {
+        Helpers.restartSystemUI();
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver

scotthartbti/android_frameworks_base

Signed-off-by: Scott <scott@ubuntu.com>

core/java/com/android/internal/util/beanstalk/AbstractAsyncSuCMDProcessor.java
@@ -1,0 +1,102 @@
+package com.android.internal.util.beanstalk;
+
+import android.os.AsyncTask;
+
+/**
+ * An abstract implentation of AsyncTask
+ *
+ * since our needs are simple send a command, perform a task when we finish
+ * this implentation requires you send the command as String...
+ * in the .execute(String) so you can send String[] of commands if needed
+ *
+ * This class is not for you if...
+ *     1) You do not need to perform any action after command execution
+ *        you want a Thread not this.
+ *     2) You need to perform more complex tasks in doInBackground
+ *        than simple script/command sequence of commands
+ *        you want your own AsyncTask not this.
+ *
+ * This class is for you if...
+ *     1) You need to run a command/script/sequence of commands without
+ *        blocking the UI thread and you must perform actions after the
+ *        task completes.
+ *     2) see #1.
+ */
+public abstract class AbstractAsyncSuCMDProcessor extends AsyncTask<String, Void, String> {
+    // if /system needs to be mounted before command
+    private boolean mMountSystem;
+    // return if we recieve a null command or empty command
+    public final String FAILURE = "failed_no_command";
+
+    /**
+     * Constructor that allows mounting/dismounting
+     * of /system partition while in background thread
+     */
+    public AbstractAsyncSuCMDProcessor(boolean mountSystem) {
+         this.mMountSystem = mountSystem;
+    }
+
+    /**
+     * Constructor that assumes /system should not be mounted
+     */
+    public AbstractAsyncSuCMDProcessor() {
+         this.mMountSystem = false;
+    }
+
+    /**
+     * DO NOT override this method you should simply send your commands off
+     * as params and expect to handle results in {@link #onPostExecute}
+     *
+     * if you find a need to @Override this method then you should
+     * consider using a new AsyncTask implentation instead
+     *
+     * @param params The parameters of the task.
+     *
+     * @return A result, defined by the subclass of this task.
+     */
+    @Override
+    protected String doInBackground(String... params) {
+        // don't bother if we don't get a command
+        if (params[0] == null || params[0].trim().equals(""))
+            return FAILURE;
+
+        String stdout = null;
+
+        // conditionally enforce mounting
+        if (mMountSystem) {
+            Helpers.getMount("rw");
+        }
+        try {
+            // process all commands ***DO NOT SEND null OR ""; you have been warned***
+            for (int i = 0; params.length > i; i++) {
+                // always watch for null and empty strings, lazy devs :/
+                if (params[i] != null && !params[i].trim().equals("")) {
+                    stdout = CMDProcessor.runSuCommand(params[i]).getStdout();
+                } else {
+                    // bail because of careless devs
+                    return FAILURE;
+                }
+            }
+        // always unmount
+        } finally {
+            if (mMountSystem)
+                Helpers.getMount("ro");
+        }
+        // return the stdout from the command
+        return stdout;
+    }
+
+    /**
+     * <p>Runs on the UI thread after {@link #doInBackground}. The
+     * specified result is the value returned by {@link #doInBackground}.</p>
+     *
+     * <p>This method won't be invoked if the task was cancelled.</p>
+     *
+     * You MUST @Override this method if you don't need the result
+     * then you should consider using a new Thread implentation instead
+     *
+     * @param result The result of the operation computed by {@link #doInBackground}.
+     */
+    @Override
+    protected abstract void onPostExecute(String result);
+}

core/java/com/android/internal/util/beanstalk/CMDProcessor.java
@@ -1,0 +1,69 @@
+package com.android.internal.util.beanstalk;
+
+import android.util.Log;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+// convenience import for quick referencing of this method
+
+public final class CMDProcessor {
+    private static final String TAG = "CMDProcessor";
+
+    private CMDProcessor() {
+        // Cannot instantiate this class
+        throw new AssertionError();
+    }
+
+    /* Run a system command with full redirection */
+    public static ChildProcess startSysCmd(String[] cmdarray, String childStdin) {
+        return new ChildProcess(cmdarray, childStdin);
+    }
+
+    public static CommandResult runSysCmd(String[] cmdarray, String childStdin) {
+        ChildProcess proc = startSysCmd(cmdarray, childStdin);
+        proc.waitFinished();
+        return proc.getResult();
+    }
+
+    public static ChildProcess startShellCommand(String cmd) {
+        String[] cmdarray = new String[3];
+        cmdarray[0] = "sh";
+        cmdarray[1] = "-c";
+        cmdarray[2] = cmd;
+        return startSysCmd(cmdarray, null);
+    }
+
+    public static CommandResult runShellCommand(String cmd) {
+        ChildProcess proc = startShellCommand(cmd);
+        proc.waitFinished();
+        return proc.getResult();
+    }
+
+    public static ChildProcess startSuCommand(String cmd) {
+        String[] cmdarray = new String[3];
+        cmdarray[0] = "su";
+        cmdarray[1] = "-c";
+        cmdarray[2] = cmd;
+        return startSysCmd(cmdarray, null);
+    }
+
+    public static CommandResult runSuCommand(String cmd) {
+        ChildProcess proc = startSuCommand(cmd);
+        proc.waitFinished();
+        return proc.getResult();
+    }
+
+    public static boolean canSU() {
+        CommandResult r = runShellCommand("id");
+        StringBuilder out = new StringBuilder(0);
+        out.append(r.getStdout());
+        out.append(" ; ");
+        out.append(r.getStderr());
+        Log.d(TAG, "canSU() su[" + r.getExitValue() + "]: " + out);
+        return r.success();
+    }
+}

core/java/com/android/internal/util/beanstalk/ChildProcess.java
@@ -1,0 +1,149 @@
+package com.android.internal.util.beanstalk;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
+import static java.lang.System.nanoTime;
+
+public class ChildProcess {
+    private String TAG = getClass().getSimpleName();
+
+    private static final int PIPE_SIZE = 1024;
+
+    private class ChildReader extends Thread {
+        InputStream mStream;
+        StringBuffer mBuffer;
+        ChildReader(InputStream is, StringBuffer buf) {
+            mStream = is;
+            mBuffer = buf;
+        }
+        public void run() {
+            byte[] buf = new byte[PIPE_SIZE];
+            try {
+                int len;
+                while ((len = mStream.read(buf)) != -1) {
+                    String s = new String(buf, 0, len);
+                    mBuffer.append(s);
+                }
+            }
+            catch (IOException e) {
+                // Ignore
+            }
+            try {
+                mStream.close();
+            }
+            catch (IOException e) {
+                // Ignore
+            }
+        }
+    }
+    private class ChildWriter extends Thread {
+        OutputStream mStream;
+        String mBuffer;
+        ChildWriter(OutputStream os, String buf) {
+            mStream = os;
+            mBuffer = buf;
+        }
+        public void run() {
+            int off = 0;
+            byte[] buf = mBuffer.getBytes();
+            try {
+                while (off < buf.length) {
+                    int len = Math.min(PIPE_SIZE, buf.length - off);
+                    mStream.write(buf, off, len);
+                    off += len;
+                }
+            }
+            catch (IOException e) {
+                // Ignore
+            }
+            try {
+                mStream.close();
+            }
+            catch (IOException e) {
+                // Ignore
+            }
+        }
+    }
+
+    private long mStartTime;
+    private Process mChildProc;
+    private ChildWriter mChildStdinWriter;
+    private ChildReader mChildStdoutReader;
+    private ChildReader mChildStderrReader;
+    private StringBuffer mChildStdout;
+    private StringBuffer mChildStderr;
+    private int mExitValue;
+    private long mEndTime;
+
+    public ChildProcess(String[] cmdarray, String childStdin) {
+        mStartTime = nanoTime();
+        try {
+            mChildProc = Runtime.getRuntime().exec(cmdarray);
+            if (childStdin != null) {
+                mChildStdinWriter = new ChildWriter(mChildProc.getOutputStream(), childStdin);
+                mChildStdinWriter.start();
+            }
+            mChildStdout = new StringBuffer();
+            mChildStdoutReader = new ChildReader(mChildProc.getInputStream(), mChildStdout);
+            mChildStdoutReader.start();
+            mChildStderr = new StringBuffer();
+            mChildStderrReader = new ChildReader(mChildProc.getErrorStream(), mChildStderr);
+            mChildStderrReader.start();
+        }
+        catch (IOException e) {
+            // XXX: log
+        }
+    }
+
+    public boolean isFinished() {
+        boolean finished = true;
+        if (mChildProc != null) {
+            try {
+                mChildProc.exitValue();
+            }
+            catch (IllegalStateException e) {
+                finished = false;
+            }
+        }
+        return finished;
+    }
+
+    public int waitFinished() {
+        while (mChildProc != null) {
+            try {
+                mExitValue = mChildProc.waitFor();
+                mEndTime = nanoTime();
+                mChildProc = null;
+                mChildStderrReader.join();
+                mChildStderrReader = null;
+                mChildStdoutReader.join();
+                mChildStdoutReader = null;
+                if (mChildStdinWriter != null) {
+                    mChildStdinWriter.join();
+                    mChildStdinWriter = null;
+                }
+            }
+            catch (InterruptedException e) {
+                // Ignore
+            }
+        }
+        return mExitValue;
+    }
+
+    public CommandResult getResult() {
+        if (!isFinished()) {
+            throw new IllegalThreadStateException("Child process running");
+        }
+        return new CommandResult(
+                mStartTime,
+                mExitValue,
+                mChildStdout.toString(),
+                mChildStderr.toString(),
+                mEndTime);
+    }
+}

services/core/java/com/android/server/power/ShutdownThread.java
@@ -79,6 +79,8 @@ import java.io.PrintWriter;
 
 import java.lang.reflect.Method;
 
+import com.android.internal.util.beanstalk.Helpers;
+
 public final class ShutdownThread extends Thread {
     // constants
     private static final String TAG = "ShutdownThread";
@@ -96,6 +98,7 @@ public final class ShutdownThread extends Thread {
     private static final int MOUNT_SERVICE_STOP_PERCENT = 20;
 
     private static final String SOFT_REBOOT = "soft_reboot";
+    private static final String SYSTEMUI_REBOOT = "systemui_reboot";
 
     // length of vibration before shutting down
     private static final int SHUTDOWN_VIBRATE_MS = 500;
@@ -257,7 +260,11 @@ public final class ShutdownThread extends Thread {
                                 if (selected != ListView.INVALID_POSITION) {
                                     String actions[] = context.getResources().getStringArray(
                                             com.android.internal.R.array.shutdown_reboot_actions);
-                                    if (selected >= 0 && selected < actions.length) {
+                                    if (actions[selected].equals(SYSTEMUI_REBOOT)) {
+                                        mReason = actions[selected];
+                                        doSystemUIReboot();
+                                        return;
+                                    } else if (selected >= 0 && selected < actions.length) {
                                         mReason = actions[selected];
                                         if (actions[selected].equals(SOFT_REBOOT)) {
                                             doSoftReboot();
@@ -294,6 +301,10 @@ public final class ShutdownThread extends Thread {
         } catch (RemoteException e) {
             Log.e(TAG, "failure trying to perform soft reboot", e);
         }
+    }
+
+    private static void doSystemUIReboot() {
+        Helpers.restartSystemUI();
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver