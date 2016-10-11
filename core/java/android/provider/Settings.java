f023541463550cc2f947e8e996ea75f8a6487b8a
UtkarshGupta <utkarsh.eminem@gmail.com>
2016-10-11 09:53:13 +0200

    FWB: Battery light: 100% charged level (1/2)
    Change-Id: I37d3ba83b6246b549f6be1096798fccd0c949f11
    Signed-off-by: Scott <scott@ubuntu.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4063,6 +4063,12 @@ public final class Settings {
         public static final String TOAST_ANIMATION = "toast_animation";
 
         /**
+         * What color to use for the battery LED while charging - really full (100%)
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_REALLY_FULL_COLOR = "battery_light_really_full_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
