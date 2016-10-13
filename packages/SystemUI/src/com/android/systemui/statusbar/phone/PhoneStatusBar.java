8e16b9564429b65917a1a9d3a698725e942aedbe
Steve Kondik <steve@cyngn.com>
2016-10-13 04:32:21 -0700

    windowmanager: Add support for blur effects
      * Support for blur layer on the lockscreen
      * Additional code for blur-behind and surface-blur has
        been removed from this commit as it is not in use.
    
      * Original work by CodeAurora / Qualcomm
      * Contributions from:
        - Steve Kondik
        - Danesh Mondegarian
        - Roman Birg
        - Clark Scheff
        - Michael Bestas
    
    Change-Id: I6285905a4e3c00dfb5471876d0cfb16b4b4e9893
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2810,6 +2810,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 }
             }
         }
+
+        if (mStatusBarWindowManager != null) {
+            mStatusBarWindowManager.setShowingMedia(hasArtwork);
+        }
         Trace.endSection();
     }
 
@@ -4136,7 +4140,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStatusBarWindowManager = new StatusBarWindowManager(mContext);
         mRemoteInputController = new RemoteInputController(mStatusBarWindowManager,
                 mHeadsUpManager);
+        mStatusBarWindowManager.setShowingMedia(mKeyguardShowingMedia);
         mStatusBarWindowManager.add(mStatusBarWindow, getStatusBarHeight());
+        mKeyguardMonitor.addCallback(mStatusBarWindowManager);
     }
 
     // called by makeStatusbar and also by PhoneStatusBarView
@@ -4364,6 +4370,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateRowStates();
         mScreenPinningRequest.onConfigurationChanged();
         mNetworkController.onConfigurationChanged();
+        mStatusBarWindowManager.onConfigurationChanged();
     }
 
     @Override
