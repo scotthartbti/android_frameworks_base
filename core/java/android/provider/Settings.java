71aa01cc3923e440f59c12165adbef7f444cdf3d
ezio84 <brabus84@gmail.com>
2016-10-15 16:37:49 +0200

    QS WiFi easy toggle: if enabled, long press for detail view
    ** DU Edit **
    Modify methods to unify all of the tile types into one singular tile -- and strings too.
    
    Change-Id: Ib3360a8afb775066dcf70119c7af7691e374e0a6
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7041,19 +7041,6 @@ public final class Settings {
         public static final String WAKE_GESTURE_ENABLED = "wake_gesture_enabled";
 
         /**
-         * Enable single click to turn Bluetooth on or off.
-         * @hide
-         */
-        public static final String QS_BT_EASY_TOGGLE = "qs_bt_easy_toggle";
-
-
-        /**
-         * Enable single click to turn WiFi on or off.
-         * @hide
-         */
-        public static final String QS_WIFI_EASY_TOGGLE = "qs_wifi_easy_toggle";
-
-        /**
          * Whether the device should doze if configured.
          * @hide
          */
@@ -7438,10 +7425,10 @@ public final class Settings {
         public static final String QS_TILES = "sysui_qs_tiles";
 
 	/**
-         * Whether to use one tap tile action to enable or disable data 
+         * Whether to use one tap QS tile action to enable or disable mobile data, wifi, and bluetooth
          * @hide
          */
-        public static final String QS_DATA_ADVANCED = "qs_data_advanced";
+        public static final String QS_EASY_TOGGLE = "qs_easy_toggle";
 
         /**
          * Whether or not the automatic storage manager is enabled and should run on the device.
