687bb501fb9987a0d6583e9c6e1b1aaf06d5017c
Marc K <morckx@gmail.com>
2016-10-19 22:26:28 +0200

    Add new importance level for blocking lock screen notifications (1/2)
    One feature that is still missing in the handling of notifications in
    N is the possibility to block them app-wise for relevance/importance
    reasons rather than for privacy reasons – i.e. also on the insecure
    lock screen.
    
    This patch plugs the hole by introducing a new importance level "very
    low" between "min" and "low" in the power notification controls that
    allows notifications on the status bar, but not on the lock screen.
    
    It brings back most of the functionality of
    Ib166db1b1673aeaea132c8eeb16c650d2f254a82 excluding the ability of
    blocking only ongoing (persistent) notifications.
    
    Change-Id: I5ceee18a3e9b507ddb51a224eddda8b38957ec75
    Signed-off-by: Marc K <morckx@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -120,6 +120,7 @@ import java.util.List;
 import java.util.Locale;
 
 import static android.service.notification.NotificationListenerService.Ranking.IMPORTANCE_HIGH;
+import static android.service.notification.NotificationListenerService.Ranking.IMPORTANCE_VERY_LOW;
 
 public abstract class BaseStatusBar extends SystemUI implements
         CommandQueue.Callbacks, ActivatableNotificationView.OnActivatedListener,
@@ -2374,7 +2375,8 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     public boolean shouldShowOnKeyguard(StatusBarNotification sbn) {
-        return mShowLockscreenNotifications && !mNotificationData.isAmbient(sbn.getKey());
+        return mShowLockscreenNotifications && !mNotificationData.isAmbient(sbn.getKey())
+            && mNotificationData.getImportance(sbn.getKey()) > IMPORTANCE_VERY_LOW;
     }
 
     protected void setZenMode(int mode) {
