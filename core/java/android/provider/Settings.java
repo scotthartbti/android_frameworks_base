cf9a09a6140e301ebc2bb98645c6f9551e632143
scotthartbti <scott.hart.bti@gmail.com>
2016-11-04 22:13:19 -0400

    Revert "Proximity speaker [1/2]"
    This reverts commit bae41a8f7324dab8edd5ccee5c462df8977756ad.
    
    Change-Id: I59d3c1a04a304cb0f7f51542ffa4b7e714892944
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3941,25 +3941,6 @@ public final class Settings {
          */
         public static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
 
-	/**
-         * Whether the proximity sensor will adjust call to speaker
-         * @hide
-         */
-        public static final String PROXIMITY_AUTO_SPEAKER = "proximity_auto_speaker";
-
-        /**
-         * Time delay to activate speaker after proximity sensor triggered
-         * @hide
-         */
-        public static final String PROXIMITY_AUTO_SPEAKER_DELAY = "proximity_auto_speaker_delay";
-
-        /**
-         * Whether the proximity sensor will adjust call to speaker,
-         * only while in call (not while ringing on outgoing call)
-         * @hide
-         */
-        public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY = "proximity_auto_speaker_incall_only";
-
         /**
          * Alternative recent apps integration using OmniSwitch
          * @hide
