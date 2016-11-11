54913aeddea0bfdd6b940f4913279beb54832866
tsubus <tsubus@ilwt.org>
2016-11-11 16:48:16 +0100

    Reduce default screenshot delay to 100ms [1/2]
    Change-Id: Ifb222c02ec9dc14872b8ceef1a538409a2f91f86

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -1764,6 +1764,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private void checkSettings() {
         mScreenshotDelay = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SCREENSHOT_DELAY, 1000);
+                Settings.System.SCREENSHOT_DELAY, 100);
     }
 }
