6c365d89351a75d562eb4e0a5cc5c1f810410d66
scotthartbti <scott.hart.bti@gmail.com>
2016-11-21 11:34:55 -0500

    Fix build
    Change-Id: I5b2fe828e80234587396fb5c52b68016fba4972d
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialog.java
@@ -685,7 +685,6 @@ public class VolumeDialog implements TunerService.Tunable {
             if (!mDynamic.get(row.stream)) {
                 mRows.remove(i);
                 mDialogContentView.removeView(row.view);
-                mDialogContentView.removeView(row.space);
             }
         }
     }
