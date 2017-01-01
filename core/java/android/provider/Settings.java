3cfc9a8ac46fc3798aeb67629ba3cdd2440f8c62
ezio84 <brabus84@gmail.com>
2016-12-31 22:50:55 -0500

    Night Light brightness mode options [1/2]
    Squash of:
    *Night tile: automatically set a lower brightness when active [1/2]
    *Night mode tile brightness: allow to set a different value [1/2]
    *Night light tile brightness: merge the two options into one [1/2]
    *Night light auto low brightness: wider action [1/2]
    *Fix manual brightness going to 0 after reboot
    
    @nathanchance edit: Changed customVal values to make more sense (0 is disabled and 3 is medium); also cleaned up alignment
    
    Change-Id: Ia431f6f204f7c8f813e93345a746a704c69e922b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7561,6 +7561,12 @@ public final class Settings {
         public static final String ONE_HANDED_MODE_UI = "one_handed_mode_ui";
 
         /**
+         * Low brightness level for the advanced night mode tile
+         * @hide
+         */
+        public static final String QS_NIGHT_BRIGHTNESS_VALUE = "qs_night_brightness_value";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
