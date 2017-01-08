453286756eba43dd40004c036663874976ced4ad
maxwen <max.weninger@gmail.com>
2017-01-08 20:47:49 +0100

    [1/3] base: custom header extensions
    add static image provider that allows using a
    specific image from a header pack
    
    Change-Id: Ie695aa9c39f8a61594c72bcd450e3cdfc02ba2c1
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4367,6 +4367,12 @@ public final class Settings {
         public static final String STATUS_BAR_CUSTOM_HEADER_PROVIDER = "status_bar_custom_header_provider";
 
         /**
+         * Manual override picture to use
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_IMAGE = "status_bar_custom_header_image";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
