c9991a8573806cfcd9c4da42f0806b6f01fc9846
LorDClockaN <lordclockan@gmail.com>
2017-01-10 13:40:58 +0100

    Add Application picker and tile for it
    PS3:
    Sort list alphabetically
    Exit picker when item is selected
    Don't exclude from recents
    
    PS4:
    Ignore case when sorting
    
    PS5:
    Add CardView
    Add icon to the activity
    
    Change-Id: I8165f6ad4f232921e16cc164923fa393fc731e40
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.external.TileServices;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
+import com.android.systemui.qs.tiles.AppPickerTile;
 import com.android.systemui.qs.tiles.BatteryTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
@@ -464,6 +465,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("locale")) return new LocaleTile(this);
         else if (tileSpec.equals("sound")) return new SoundTile(this);
         else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
+        else if (tileSpec.equals("app_picker")) return new AppPickerTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
