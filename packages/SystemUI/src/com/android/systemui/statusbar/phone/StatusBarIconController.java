9e2097d6224e9779fdb1939f53c6e0937371d2bd
scotthartbti <scott.hart.bti@gmail.com>
2017-03-12 23:33:29 -0400

    missing import
    Change-Id: I081a17318cae3ac8002d228aa898f4622c08f3b1
    Signed-off-by: scotthartbti <scott.hart.bti@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -36,6 +36,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.systemui.BatteryLevelTextView;
