dd09e3ff5e31f0b579c7193f30402051d0b3d427
Ali B <abittin@gmail.com>
2017-06-04 00:29:16 +0300

    Update memory state on emtpyView click
    Displaying correct memory state is better than displaying
    misleading information. Update memory state on clicking emtpyview too
    
    Change-Id: I766eb113be82ee50d9ba8b443e354facda6c2d59

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -338,6 +338,7 @@ public class RecentsView extends FrameLayout {
             @Override
             public void onClick(View view) {
                 EventBus.getDefault().send(new ToggleRecentsEvent());
+                updateMemoryStatus();
             }
         });
     }
